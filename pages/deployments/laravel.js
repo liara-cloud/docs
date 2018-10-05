70c2648ca2e87796785f97963c6e7502092d23ab
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2018-10-05 18:21:28 +0330

    wip

---
+++ pages/deployments/laravel.js
@@ -0,0 +1,14 @@
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout>
+    <h1>استقرار پروژه‌های Laravel</h1>
+
+    <p>
+      پروژه‌ی شما باید دارای فایل
+      <span className="code">composer.json</span>
+      باشد تا بتواند در لیارا مستقر شود.
+    </p>
+    
+  </Layout>
+)
