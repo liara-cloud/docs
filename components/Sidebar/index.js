f32cf0004b9cd7acabc4111c26cf05ace3b04067
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2019-10-06 21:02:55 +0330

    Make it responsive

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,81 +1,103 @@
+import React, { Component } from 'react';
+
 import './style.css'
 import ActiveLink from './ActiveLink'
 
-export default () => (
-  <div className="sidebar">
-    <a href="https://liara.ir" className="brand">
-      <img src="/static/logo.png" alt="liara" className="logo" />
-      <div className="brand__text">
-        <h2>سرویس ابری لیارا</h2>
-        <p>ارائه دهنده خدمات ابری PaaS و DBaaS</p>
+export default class Sidebar extends Component {
+  state = {
+    isNavOpen: false,
+  };
+
+  toggleNav = () => {
+    this.setState(prevState => ({ isNavOpen: ! prevState.isNavOpen }));
+  };
+
+  render() {
+    const { isNavOpen } = this.state;
+    return (
+      <div className="sidebar">
+        <a href="https://liara.ir" className="brand">
+          <img src="/static/logo.png" alt="liara" className="logo" />
+          <div className="brand__text">
+            <h2>سرویس ابری لیارا</h2>
+            <p>ارائه دهنده خدمات ابری PaaS و DBaaS</p>
+          </div>
+        </a>
+
+        <button type="button" className="menu-button" onClick={this.toggleNav}>
+          <img src="/static/icons/menu.svg" alt="" className="menu-button__icon" />
+          فهرست
+        </button>
+
+        <div className={isNavOpen ? '' : 'hide-nav'}>
+          <nav className="nav">
+            <ul className="nav__list">
+              <li><ActiveLink href="/">آشنایی با لیارا</ActiveLink></li>
+            </ul>
+          </nav>
+
+          <nav className="nav">
+            <span className="nav__title">پروژه‌ها</span>
+            <ul className="nav__list">
+              <li><ActiveLink href="/projects/logs">لاگ‌ها</ActiveLink></li>
+              <li><ActiveLink href="/projects/console">کنسول</ActiveLink></li>
+              <li><ActiveLink href="/projects/environment-variables">متغیرها (تنظیمات)</ActiveLink></li>
+              <li><ActiveLink href="/projects/volumes">ذخیره‌ی داده‌ها</ActiveLink></li>
+              <li><ActiveLink href="/projects/ignore">نادیده‌گرفتن فایل‌ها</ActiveLink></li>
+              <li><ActiveLink href="/projects/fixed-ip">آی‌پی ثابت</ActiveLink></li>
+            </ul>
+          </nav>
+
+          <nav className="nav">
+            <span className="nav__title">کلاینت‌ها</span>
+            <ul className="nav__list">
+              <li><ActiveLink href="/clients/cli">CLI</ActiveLink></li>
+              <li><ActiveLink href="/clients/api">API</ActiveLink></li>
+            </ul>
+          </nav>
+
+          <nav className="nav">
+            <span className="nav__title">استقرار پروژه‌ها</span>
+            <ul className="nav__list">
+              <li><ActiveLink icon="react.svg" href="/deployments/react">React</ActiveLink></li>
+              <li><ActiveLink icon="vue.svg" href="/deployments/vue">Vue</ActiveLink></li>
+              <li><ActiveLink icon="angularjs.svg" href="/deployments/angular">Angular</ActiveLink></li>
+              <li><ActiveLink icon="HTML5.svg" href="/deployments/static">Static</ActiveLink></li>
+              <li><ActiveLink icon="nodejs.svg" href="/deployments/nodejs">NodeJS</ActiveLink></li>
+              <li><ActiveLink icon="php.svg" href="/deployments/php">PHP</ActiveLink></li>
+              <li><ActiveLink icon="laravel.svg" href="/deployments/laravel">Laravel</ActiveLink></li>
+              <li><ActiveLink icon="django.svg" href="/deployments/django">Django</ActiveLink></li>
+              <li><ActiveLink icon="flask.svg" href="/deployments/flask">Flask</ActiveLink></li>
+              <li><ActiveLink icon="wordpress.svg" href="/deployments/wordpress">WordPress</ActiveLink></li>
+              <li><ActiveLink icon="docker.png" href="/deployments/docker">Docker</ActiveLink></li>
+            </ul>
+          </nav>
+
+          {/* TODO: <nav className="nav">
+          <span className="nav__title">دامنه‌ها</span>
+          <ul className="nav__list">
+            <li><ActiveLink href="/domains/mysql">افزودن دامنه</ActiveLink></li>
+          </ul>
+        </nav> */}
+
+          {/* TODO: <nav className="nav">
+          <span className="nav__title">دیتابیس‌ها</span>
+          <ul className="nav__list">
+            <li><ActiveLink href="/databases/mysql">MySQL</ActiveLink></li>
+            <li><ActiveLink href="/databases/mongodb">MongoDB</ActiveLink></li>
+          </ul>
+        </nav> */}
+
+          <nav className="nav">
+            <span className="nav__title">سرویس ذخیره‌ی فایل</span>
+            <ul className="nav__list">
+              <li><ActiveLink href="/storage/overview">آشنایی</ActiveLink></li>
+              <li><ActiveLink href="/storage/nodejs">NodeJS SDK</ActiveLink></li>
+              <li><ActiveLink href="/storage/laravel">Laravel S3 Driver</ActiveLink></li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </a>
-
-    <nav className="nav">
-      <ul className="nav__list">
-        <li><ActiveLink href="/">آشنایی با لیارا</ActiveLink></li>
-      </ul>
-    </nav>
-
-    <nav className="nav">
-      <span className="nav__title">پروژه‌ها</span>
-      <ul className="nav__list">
-        <li><ActiveLink href="/projects/logs">لاگ‌ها</ActiveLink></li>
-        <li><ActiveLink href="/projects/console">کنسول</ActiveLink></li>
-        <li><ActiveLink href="/projects/environment-variables">متغیرها (تنظیمات)</ActiveLink></li>
-        <li><ActiveLink href="/projects/volumes">ذخیره‌ی داده‌ها</ActiveLink></li>
-        <li><ActiveLink href="/projects/ignore">نادیده‌گرفتن فایل‌ها</ActiveLink></li>
-        <li><ActiveLink href="/projects/fixed-ip">آی‌پی ثابت</ActiveLink></li>
-      </ul>
-    </nav>
-
-    <nav className="nav">
-      <span className="nav__title">کلاینت‌ها</span>
-      <ul className="nav__list">
-        <li><ActiveLink href="/clients/cli">CLI</ActiveLink></li>
-        {/* <li><ActiveLink href="/clients/api">API</ActiveLink></li> */}
-      </ul>
-    </nav>
-
-    <nav className="nav">
-      <span className="nav__title">استقرار پروژه‌ها</span>
-      <ul className="nav__list">
-        <li><ActiveLink icon="react.svg" href="/deployments/react">React</ActiveLink></li>
-        <li><ActiveLink icon="vue.svg" href="/deployments/vue">Vue</ActiveLink></li>
-        <li><ActiveLink icon="angularjs.svg" href="/deployments/angular">Angular</ActiveLink></li>
-        <li><ActiveLink icon="HTML5.svg" href="/deployments/static">Static</ActiveLink></li>
-        <li><ActiveLink icon="nodejs.svg" href="/deployments/nodejs">NodeJS</ActiveLink></li>
-        <li><ActiveLink icon="php.svg" href="/deployments/php">PHP</ActiveLink></li>
-        <li><ActiveLink icon="laravel.svg" href="/deployments/laravel">Laravel</ActiveLink></li>
-        <li><ActiveLink icon="django.svg" href="/deployments/django">Django</ActiveLink></li>
-        <li><ActiveLink icon="flask.svg" href="/deployments/flask">Flask</ActiveLink></li>
-        <li><ActiveLink icon="wordpress.svg" href="/deployments/wordpress">WordPress</ActiveLink></li>
-        <li><ActiveLink icon="docker.png" href="/deployments/docker">Docker</ActiveLink></li>
-      </ul>
-    </nav>
-
-    {/* TODO: <nav className="nav">
-      <span className="nav__title">دامنه‌ها</span>
-      <ul className="nav__list">
-        <li><ActiveLink href="/domains/mysql">افزودن دامنه</ActiveLink></li>
-      </ul>
-    </nav> */}
-
-    {/* TODO: <nav className="nav">
-      <span className="nav__title">دیتابیس‌ها</span>
-      <ul className="nav__list">
-        <li><ActiveLink href="/databases/mysql">MySQL</ActiveLink></li>
-        <li><ActiveLink href="/databases/mongodb">MongoDB</ActiveLink></li>
-      </ul>
-    </nav> */}
-
-    <nav className="nav">
-      <span className="nav__title">سرویس ذخیره‌ی فایل</span>
-      <ul className="nav__list">
-        <li><ActiveLink href="/storage/overview">آشنایی</ActiveLink></li>
-        <li><ActiveLink href="/storage/nodejs">NodeJS SDK</ActiveLink></li>
-        <li><ActiveLink href="/storage/laravel">Laravel S3 Driver</ActiveLink></li>
-      </ul>
-    </nav>
-  </div>
-)
+    );
+  }
+}
