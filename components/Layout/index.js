89935a16e5c596f1461074f13dda3d314276de77
mtalkhabi <mtalkhabi33@gmail.com>
2019-10-06 23:49:02 +0330

    Minor improve in style

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -9,12 +9,11 @@ import Sidebar from '../Sidebar'
 export default ({ children }) => (
   <Fragment>
     <Head>
-      <meta name="theme-color" content="#000000" />
+      <meta name="theme-color" content="#2C333F" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     </Head>
 
-    <div className="top-bar"></div>
     <div className="wrapper">
       <Sidebar />
       <article className="article">{children}</article>
