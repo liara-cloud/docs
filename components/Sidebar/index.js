a079cf5d8a626dcd1bb6cef12e62e6ad69826531
reza <kinggarethbale@gmail.com>
2020-09-08 22:54:52 +0430

    File System section added to app features.

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -168,8 +168,9 @@ export default class Sidebar extends Component {
           </nav>
 
           <nav className="nav">
-            <NavTitle href="/app-features">امکانات برنامه</NavTitle>
+            <NavTitle href="/app-features">ویژگی‌های برنامه</NavTitle>
             <ul className="nav__list">
+              <li><ActiveLink href="/app-features/file-system">فایل سیستم</ActiveLink></li>
               <li><ActiveLink href="/app-features/logs">لاگ‌ها</ActiveLink></li>
               <li><ActiveLink href="/app-features/environment-variables">متغیرهای محیطی (env ها)</ActiveLink></li>
               <li><ActiveLink href="/app-features/console">خط فرمان</ActiveLink></li>
