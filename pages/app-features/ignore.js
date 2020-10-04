b4b0bec1d73cb2d1406b6b1f535fad472a8a8152
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-10-04 11:05:04 +0330

    Add default ignored patterns

--- pages/app-features/ignore.js
+++ pages/app-features/ignore.js
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layout";
 import Head from "next/head";
+import Notice from "../../components/Notice";
 
 export default () => (
   <Layout>
@@ -48,5 +49,49 @@ export default () => (
       <span className="code">.gitignore</span>
       خوانده نمی‌شوند.
     </p>
+
+    <h3>پوشه‌ها و فایل‌هایی که به‌صورت پیش‌فرض نادیده گرفته می‌شوند</h3>
+    <p>
+      برخی پوشه‌ها و فایل‌ها هستند که در اکثر برنامه‌ها باید نادیده گرفته شوند.
+      Liara CLI
+      بدون در نظر گرفتن این‌که شما پوشه‌های زیر را در <span className="code">.gitignore</span>
+      قرار داده‌اید یا خیر، از آن‌ها صرف نظر می‌کند و این پوشه‌ها و فایل‌ها به سرور آپلود نمی‌شوند:
+    </p>
+    <p>
+      <ul style={{ direction: 'ltr' }}>
+        <li>.git</li>
+        <li>.idea</li>
+        <li>.vscode</li>
+        <li>.gitignore</li>
+        <li>.liaraignore</li>
+        <li>.dockerignore</li>
+        <li>*.*~</li>
+        <li>liara.json</li>
+        <li>node_modules</li>
+        <li>bower_components</li>
+      </ul>
+    </p>
+    <p>
+      اما شما می‌توانید عکس این الگوها و در واقع pattern ها را به فایل
+      <span className="code">.gitignore</span>
+      اضافه کنید و Liara CLI
+      را مجبور کنید که آن‌ها را آپلود کند. برای مثال، اگر قصد دارید که Liara CLI
+      پوشه‌ی <span className="code">bower_components</span>
+      را حتما آپلود کند، باید چنین خطی را به <span className="code">.gitignore</span>
+      اضافه کنید:
+    </p>
+    <pre>
+      <code>!bower_components</code>
+    </pre>
+    <p>
+      در واقع، علامت تعجبی که در ابتدای نام پوشه قرار دارد، عملکرد
+      ignore را عکس می‌کند و آن پوشه را حتما به لیست آپلود اضافه می‌کند و از لیست ignore
+      پیش‌فرض خارج می‌کند.
+    </p>
+    <Notice variant="warning">
+      پیشنهاد می‌کنیم هیچ‌وقت <span className="code">node_modules</span>
+      را به‌سرور آپلود نکنید. چرا که سرعت استقرار را بسیار کند خواهد کرد و ممکن است حتی
+      عملیات استقرار با خطا مواجه شود.
+    </Notice>
   </Layout>
 );
