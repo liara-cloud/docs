2ecf592d56b6ef850224e7dcefb90053d79d3f0e
M . A . Dehghani <m.a.dehghani609@gmail.com>
2021-08-08 11:34:26 +0430

    add private network to docs

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -193,6 +193,7 @@ export default class Sidebar extends Component {
               <li><ActiveLink href="/app-features/health-check">بررسی سلامت</ActiveLink></li>
               <li><ActiveLink href="/app-features/applets">برنامک‌ها</ActiveLink></li>
               <li><ActiveLink href="/app-features/email">ایمیل</ActiveLink></li>
+              <li><ActiveLink href="/app-features/private-network">شبکه‌ی خصوصی</ActiveLink></li>
             </ul>
           </nav>
 
