34b8f00b8c16525e97046eb8adc2a27f72441c20
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2021-08-16 12:59:19 +0430

    Add laravel voyager docs (#44)
    * Add laravel voyager docs
    
    * Minor improvement: laravel voyager

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -251,6 +251,7 @@ export default class Sidebar extends Component {
               <li><ActiveLink href="/instructions/grafana">Grafana</ActiveLink></li>
               <li><ActiveLink href="/instructions/prestashop">Prestashop</ActiveLink></li>
               <li><ActiveLink href="/instructions/mattermost">Mattermost</ActiveLink></li>
+              <li><ActiveLink href="/instructions/voyager">Voyager</ActiveLink></li>
             </ul>
           </nav>
 
