54c96551c453f74efe19fec8659fefe612ef3901
M . A . Dehghani <m.a.dehghani609@gmail.com>
2021-08-16 12:22:29 +0430

    Add laravel voyager docs

---
+++ pages/instructions/voyager.js
@@ -0,0 +1,44 @@
+import Layout from "../../components/Layout"
+import Head from "next/head"
+import Link from "next/link"
+import Highlight from "react-highlight"
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>
+                استفاده از Voyager در برنامه‌های Laravel - سرویس ابری لیارا
+            </title>
+        </Head>
+
+        <h1>استفاده از Voyager در برنامه‌های Laravel</h1>
+        <p>شما برای استفاده از پکیج <a href="https://github.com/the-control-group/voyager" target="_blank">Voyager</a> در برنامه‌های Laravel باید در قدم اول پکیج <span className="code">tcg/voyager</span> را در بخش <span className="code">dont-discover</span> فایل <span className="code">composer.json</span> برنامه‌ی خود اضافه کنید:</p>
+        <Highlight className="json">{`"extra": {
+    "laravel": {
+        "dont-discover": [
+            "tcg/voyager"
+        ]
+    }
+},`}</Highlight>
+
+        <p>سپس  در قدم بعد باید فایل <span className="code">app/config.php</span> را به شکل زیر تغییر دهید تا <span className="code">VoyagerServiceProvider</span> و <span className="code">VoyagerDummyServiceProvider</span> فقط در حالت production به پیکربندی برنامه اضافه شوند:</p>
+
+        <Highlight className="php">{`<?php
+
+$results = [
+
+    /*
+    ** default config
+    */
+    
+];
+
+if (env('APP_ENV') === 'production') {
+    $results['providers'][] = "TCG\\Voyager\\VoyagerServiceProvider";
+    $results['providers'][] = "TCG\\Voyager\\Providers\\VoyagerDummyServiceProvider";
+}
+
+return $results;`}</Highlight>
+
+    </Layout>
+);
