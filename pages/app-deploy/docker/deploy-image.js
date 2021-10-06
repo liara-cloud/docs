ff943cd396f595b593e95767a82fbd52caa8c3a1
userdehghani <m.a.dehghani609@gmail.com>
2021-10-06 14:14:54 +0330

    do some improvements

--- pages/app-deploy/docker/deploy-image.js
+++ pages/app-deploy/docker/deploy-image.js
@@ -1,8 +1,8 @@
-import Layout from "../../../components/Layout";
-import Notice from "../../../components/Notice";
-import Head from "next/head";
-import Link from "next/link";
-import Highlight from "react-highlight";
+import Layout from '../../../components/Layout';
+import Notice from '../../../components/Notice';
+import Head from 'next/head';
+import Link from 'next/link';
+import Highlight from 'react-highlight';
 
 export default () => (
     <Layout>
@@ -23,8 +23,8 @@ export default () => (
         </div>
 
         <p>
-            در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی آموزشی زیر
-            ‌را مشاهده کنید.
+            در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی
+            آموزشی زیر ‌را مشاهده کنید.
         </p>
         <video
             src="https://files.liara.ir/liara/docker-image.mp4"
@@ -33,19 +33,15 @@ export default () => (
             width="100%"
         ></video>
         <p>
-
-            برای استقرار مستقیم imageهای build شده از DockerHub یا هر رجیستری‌های عمومی دیگر در لیارا تنها کافیست که به شکل زیر از پارامتر <span className="code">--image</span> در زمان اجرای دستور <span className="code">liara deploy</span> استفاده کنید:
-        </p>
-        <Highlight className="bash">
-            {`$ liara deploy --image getmeili/meilisearch:v0.22 --app search-app --port 7700`}
-        </Highlight>
-
-        <Notice variant="warning">
-            توصیه می‌کنیم حتما در زمان استقرار image از تگ مشخصی استفاده کرده و تا حد امکان از تگ <span className="code">latest</span> استفاده نکنید چون با تغییر نسخه‌ی image ممکن است برنامه‌ی شما دچار مشکل شود.
-        </Notice>
-
-        <p>
-            روش دیگر این است که یک فایل با نام <Link href="/app-deploy/docker/liarajson">liara.json</Link> در مسیر اصلی پروژه ایجاد کرده و پس از تنظیم مشخصه‌های مورد نیاز مانند قطعه کد زیر، دستور <span className="code">liara deploy</span>
+            برای استقرار مستقیم imageهای build شده از DockerHub یا هر
+            رجیستری‌های عمومی دیگر در لیارا می‌توانید از پارامتر{' '}
+            <span className="code">--image</span> در زمان اجرای دستور{' '}
+            <span className="code">liara deploy</span> استفاده کنید.
+            روش دیگر این است
+            که یک فایل با نام{' '}
+            <Link href="/app-deploy/docker/liarajson">liara.json</Link> در مسیر
+            اصلی پروژه ایجاد کرده و پس از تنظیم مشخصه‌های مورد نیاز مانند قطعه
+            کد زیر، دستور <span className="code">liara deploy</span>
             را اجرا کنید:
         </p>
         <Highlight className="json">
@@ -62,13 +58,17 @@ export default () => (
 }
 `}
         </Highlight>
+
+        <Notice variant="warning">
+            توصیه می‌کنیم حتما در زمان استقرار image از تگ مشخصی استفاده کرده و
+            تا حد امکان از تگ <span className="code">latest</span> استفاده نکنید
+            چون با تغییر نسخه‌ی image ممکن است برنامه‌ی شما دچار مشکل شود.
+        </Notice>
+
         <p>
             توجه داشته باشید که در لیارا به‌جای مفهوم Volume از مفهوم Disk
-            استفاده می‌کنیم که کاربردهای آن‌ها کاملا یکسان است.
-            {' '}
-            <Link href="/app-deploy/docker/disks">
-                توضیحات بیشتر
-            </Link>
+            استفاده می‌کنیم که کاربردهای آن‌ها کاملا یکسان است.{' '}
+            <Link href="/app-deploy/docker/disks">توضیحات بیشتر</Link>
         </p>
         <Link href="/app-deploy/docker/envs">متوجه شدم، برو گام بعدی!</Link>
     </Layout>
