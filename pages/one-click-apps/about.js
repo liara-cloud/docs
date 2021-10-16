6107bf48cdbeaee45ac9c657272d8fece4bdfe08
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2021-10-16 12:28:51 +0330

    improve documents (#91)

--- pages/one-click-apps/about.js
+++ pages/one-click-apps/about.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
-import ZoomableImage from "../../components/ZoomableImage";
+import Link from "next/link";
 
 export default () => (
   <Layout>
@@ -13,23 +13,68 @@ export default () => (
 
     <h1>درباره برنامه‌های آماده</h1>
     <p>
-      در صفحه‌ی ساخت برنامه بخشی وجود دارد به نام <b>برنامه‌های آماده</b> که شما می‌توانید به سادگی و راحتی برنامه‌های
-      کاربردی خاصی را فقط با یک کلیک ایجاد کنید. برای مثال شما می‌توانید با یک
-      کلیک WordPress را نصب و اجرا کنید. دیتابیس به صورت خودکار برای شما ایجاد و
-      WordPress به آن متصل می‌شود.
+      در صفحه‌ی ساخت برنامه، بخشی با نام <b>برنامه‌های آماده</b> وجود دارد
+      که شما می‌توانید برنامه‌های کاربردی خاصی را به سادگی و فقط با
+      یک کلیک ایجاد کنید. برای مثال شما می‌توانید با یک کلیک WordPress را
+      نصب و اجرا کنید. دیتابیس به صورت خودکار برای شما ایجاد و WordPress
+      به آن متصل می‌شود.
     </p>
-    <ZoomableImage src="/static/one-click-apps.png" />
 
-    <p>در حال حاضر این برنامه‌ها در این بخش وجود دارد:</p>
-    <ul dir="ltr">
-      <li>WordPress</li>
-      <li>Ghost</li>
-      <li>RocketChat</li>
-      <li>Gitea</li>
-      <li>Metabase</li>
-      <li>Chrome</li>
-      <li>NextCloud</li>
-      <li>Pusher</li>
-    </ul>
+    <h3>برنامه‌های آماده‌ای که ارائه می‌کنیم</h3>
+    <p>
+      برای مطالعه‌ی راهنمای نصب هر برنامه آماده، صفحه‌ی مربوط به آن را دنبال کنید.
+    </p>
+
+    <div className="platforms">
+      <Link href="/one-click-apps/wordpress/install">
+        <a>
+          <img src="/static/platformicons/wordpress.svg" alt="wordpress" />
+          <span>WordPress</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/pusher/install">
+        <a>
+          <img src="/static/platformicons/pusher.svg" alt="pusher" />
+          <span>Pusher</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/others">
+        <a>
+          <img src="/static/platformicons/ghost.svg" alt="ghost" />
+          <span>Ghost</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/rocketchat">
+        <a>
+          <img src="/static/platformicons/rocketchat.svg" alt="rocketchat" />
+          <span>Rocket.Chat</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/gitea">
+        <a>
+          <img src="/static/platformicons/gitea.svg" alt="gitea" />
+          <span>Gitea</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/metabase">
+        <a>
+          <img src="/static/platformicons/metabase.svg" alt="metabase" />
+          <span>Metabase</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/headless-chrome/install">
+        <a>
+          <img src="/static/platformicons/chrome.svg" alt="Chrome" />
+          <span>Chrome</span>
+        </a>
+      </Link>
+      <Link href="/one-click-apps/nextcloud">
+        <a>
+          <img src="/static/platformicons/nextcloud.svg" alt="nextcloud" />
+          <span>NextCloud</span>
+        </a>
+      </Link>
+    </div>
+
   </Layout>
 );
