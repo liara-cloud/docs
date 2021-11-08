000e9dabeaa3bda5db12e1b8ef8b9e1af178483e
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2021-11-08 11:59:06 +0330

    Add new liara-cli commands (#96)

---
+++ pages/cli/account.js
@@ -0,0 +1,160 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Highlight from 'react-highlight';
+import Layout from '../../components/Layout';
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>
+                مستندات مدیریت حساب‌های کاربری در رابط خط فرمان لیارا Liara CLI -
+                سرویس ابری لیارا
+            </title>
+        </Head>
+
+        <div className="page-head">
+            <div className="page-title">
+                <h1>رابط خط فرمان لیارا</h1>
+                <span className="page-description">(Liara CLI)</span>
+            </div>
+        </div>
+
+        <h4>مدیریت حساب‌های کاربری</h4>
+        <Highlight className="bash">{`$ liara account`}</Highlight>
+        <p>
+            با اجرای این دستور می‌توانید دستورات مرتبط با مدیریت حساب‌های کاربری را مشاهده کنید.
+        </p>
+
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;account:add</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. <Link href="#add">اضافه کردن یک حساب کاربری جدید</Link>
+            </p>
+
+            <li>&nbsp;&nbsp;account:list</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. <Link href="#list">مشاهده‌ی لیستی از حساب‌های اضافه شده</Link>
+            </p>
+
+            <li>&nbsp;&nbsp;account:remove</li>
+            <p dir="rtl" className="commandDescription">
+                ۳. <Link href="#remove">حذف یکی از حساب‌های اضافه شده</Link>
+            </p>
+
+            <li>&nbsp;&nbsp;account:use</li>
+            <p dir="rtl" className="commandDescription">
+                ۴. <Link href="#use">انتخاب یکی از حساب‌های اضافه شده به‌عنوان حساب اصلی</Link>
+            </p>
+
+        </ol>
+
+        <h4 id="add">اضافه کردن یک حساب کاربری جدید</h4>
+        <Highlight className="bash">{`$ liara account:add`}</Highlight>
+        <p>
+            این دستور  از شما نام انتخابی، موقعیت مکانی، ایمیل و رمز عبور حساب ایجاد شده را می‌پرسد.
+        </p>
+
+        <h5>دستور <span className="code">liara account:add</span> این پارامتر‌ها را می‌پذیرد</h5>
+
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;-a, --account</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. نام انتخابی شما
+            </p>
+
+            <li>&nbsp;&nbsp;-e, --email</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. ایمیل حساب کاربری ایجاد شده در لیارا
+            </p>
+
+            <li>&nbsp;&nbsp;-h, --help</li>
+            <p dir="rtl" className="commandDescription">
+                ۳. نمایش راهنما
+            </p>
+
+            <li>&nbsp;&nbsp;-p, --password</li>
+            <p dir="rtl" className="commandDescription">
+                ۴. رمز عبور حساب کاربری ایجاد شده در لیارا
+            </p>
+
+            <li>&nbsp;&nbsp;--api-token=</li>
+            <p dir="rtl" className="commandDescription">
+                ۵. اضافه کردن یک حساب کاربری جدید به کمک{' '}
+                <a href="https://console.liara.ir/API" target="_blank">
+                    api token
+                </a>{' '}
+                بدون ورود به حساب کاربری
+            </p>
+
+            <li>&nbsp;&nbsp;--region=iran|germany</li>
+            <p dir="rtl" className="commandDescription">
+                ۶. مشخص‌کردن موقعیت جغرافیایی (مناسب ci/cd)
+            </p>
+        </ol>
+
+        <h4 id="list">مشاهده‌ی لیستی از حساب‌های اضافه شده</h4>
+        <Highlight className="bash">{`$ liara account:list`}</Highlight>
+
+        <h5>دستور <span className="code">liara account:list</span> یا به اختصار <span className="code">liara account:ls</span> این پارامتر‌ها را می‌پذیرد</h5>
+
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;-h, --help</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. نمایش راهنما
+            </p>
+
+            <li>&nbsp;&nbsp;--csv</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. ارائه خروجی CSV از لیست حساب‌های اضافه شده
+            </p>
+
+            <li>&nbsp;&nbsp;--sort</li>
+            <p dir="rtl" className="commandDescription">
+                ۳. مرتب سازی لیست حساب‌های اضافه شده براساس Name, Email, Region, Current
+            </p>
+
+            <li>&nbsp;&nbsp;--region=iran|germany</li>
+            <p dir="rtl" className="commandDescription">
+                4. مشخص‌کردن موقعیت جغرافیایی
+            </p>
+        </ol>
+
+        <h4 id="remove">حذف یکی از حساب‌های اضافه شده</h4>
+        <Highlight className="bash">{`$ liara account:remove`}</Highlight>
+
+        <h5>دستور <span className="code">liara account:remove</span> یا به اختصار <span className="code">liara account:rm</span> این پارامتر‌ها را می‌پذیرد</h5>
+
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;-a, --account</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. نام حسابی که می‌خواهید آن را حذف کنید
+            </p>
+
+            <li>&nbsp;&nbsp;-h, --help</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. نمایش راهنما
+            </p>
+        </ol>
+
+        <h4 id="use">انتخاب یکی از حساب‌های اضافه شده به‌عنوان حساب اصلی</h4>
+        <Highlight className="bash">{`$ liara account:use`}</Highlight>
+
+        <h5>دستور <span className="code">liara account:use</span> این پارامتر‌ها را می‌پذیرد</h5>
+
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;-a, --account</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. نام حسابی که می‌خواهید آن را به‌عنوان حساب اصلی استفاده کنید
+            </p>
+
+            <li>&nbsp;&nbsp;-h, --help</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. نمایش راهنما
+            </p>
+        </ol>
+
+        <br />
+        <Link href="/cli/commands">متوجه شدم، برو گام بعدی!</Link>
+
+    </Layout>
+);
---
+++ pages/cli/deploy.js
@@ -0,0 +1,97 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Highlight from 'react-highlight';
+import Layout from '../../components/Layout';
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>
+                مستندات استقرار برنامه در رابط خط فرمان لیارا Liara CLI - سرویس
+                ابری لیارا
+            </title>
+        </Head>
+
+        <div className="page-head">
+            <div className="page-title">
+                <h1>رابط خط فرمان لیارا</h1>
+                <span className="page-description">(Liara CLI)</span>
+            </div>
+        </div>
+
+        <h4>استقرار برنامه</h4>
+        <Highlight className="bash">{`$ liara deploy`}</Highlight>
+        <p>
+            این دستور از شما شناسه برنامه‌ای که قصد دارید پروژه‌تان در آن مستقر
+            شود را می‌پرسد.
+        </p>
+
+        <h5>دستور <span className="code">liara deploy</span> این پارامتر‌ها را می‌پذیرد</h5>
+        <ol dir="ltr">
+            <li>&nbsp;&nbsp;-a, --app</li>
+            <p dir="rtl" className="commandDescription">
+                ۱. شناسه برنامه‌ای که قصد دارید پروژه‌تان در آن مستقر شود
+            </p>
+
+            <li>&nbsp;&nbsp;-d, --disks</li>
+            <p dir="rtl" className="commandDescription">
+                ۲. اتصال یک یا چند دیسک به مسیرهای مورد نظر، برای مثال:
+                disk-name:/var/www/html
+            </p>
+
+            <li>&nbsp;&nbsp;-h, --help</li>
+            <p dir="rtl" className="commandDescription">
+                ۳. نمایش راهنما
+            </p>
+
+            <li>&nbsp;&nbsp;--image</li>
+            <p dir="rtl" className="commandDescription">
+                ۴. استقرار مستقیم Image از DockerHub
+            </p>
+
+            <li>&nbsp;&nbsp;-m, --message</li>
+            <p dir="rtl" className="commandDescription">
+                ۵. در نظر گرفتن پیامی برای این استقرار، مثلا fix the user login
+                bug
+            </p>
+
+            <li>&nbsp;&nbsp;--api-token=</li>
+            <p dir="rtl" className="commandDescription">
+                ۶. اجرای آنی عملیات استقرار به کمک{' '}
+                <a href="https://console.liara.ir/API" target="_blank">
+                    api token
+                </a>{' '}
+                بدون ورود به حساب کاربری (مناسب ci/cd)
+            </p>
+
+            <li>&nbsp;&nbsp;--detach</li>
+            <p dir="rtl" className="commandDescription">
+                ۶. عدم نمایش لاگ‌ها بعد از هر استقرار
+            </p>
+
+            <li>&nbsp;&nbsp;--path</li>
+            <p dir="rtl" className="commandDescription">
+                ۷. آدرس ریشه پروژه‌ای که قصد استقرار آن را دارید
+            </p>
+
+            <li>&nbsp;&nbsp;--platform</li>
+            <p dir="rtl" className="commandDescription">
+                ۸. مشخص کردن پلتفرم [node, laravel, php, django, flask,
+                netcore, react, angular, vue, static, docker]
+            </p>
+
+            <li>&nbsp;&nbsp;--port=port</li>
+            <p dir="rtl" className="commandDescription">
+                ۹. مشخص کردن port ای که برنامه شما به آن گوش می‌دهد.
+            </p>
+
+            <li>&nbsp;&nbsp;--region=iran|germany</li>
+            <p dir="rtl" className="commandDescription">
+                ۱۰. مشخص‌کردن موقعیت جغرافیایی (مناسب ci/cd)
+            </p>
+        </ol>
+
+        <br />
+        <Link href="/cli/app">متوجه شدم، برو گام بعدی!</Link>
+    </Layout>
+);
