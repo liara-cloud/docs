3cefe6ce9b066ff6eabc88253f79dfff90ad2cf2
gholi-dev <aligholi8220@gmail.com>
2021-11-30 11:16:11 +0330

    Add poster for Asciinema player

--- components/Asciinema.js
+++ components/Asciinema.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function Asciinema({id}) {
+export default function Asciinema({ id }) {
+  const [poster, setPoster] = useState(true);
+  const handlePoster = () => {
+    setPoster(false);
+  };
+
+  const display = poster ? "block" : "none";
   return (
     <div>
+      <div className="asciinema_box">
+        <img
+          onClick={handlePoster}
+          style={{ display: display }}
+          className="asciinema_poster"
+          src={`https://asciinema.org/a/${id}.svg`}
+        />
+      </div>
       <iframe
-        className="asciinema"
+        className="asciinema_video"
         allowfullscreen="true"
         webkitallowfullscreen="true"
         mozallowfullscreen="true"
