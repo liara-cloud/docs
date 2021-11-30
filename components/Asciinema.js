a169bd035ecdeb08fd673ce5e556e594482059cd
ali gholi <59848329+gholi-dev@users.noreply.github.com>
2021-11-30 12:11:13 +0330

    create Asciinema components

---
+++ components/Asciinema.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function Asciinema({ id }) {
+  const [poster, setPoster] = useState(true);
+  const handlePoster = () => {
+    setPoster(false);
+  };
+
+  const display = poster ? "block" : "none";
+  return (
+    <div>
+      <div className="asciinema_box">
+        <img
+          onClick={handlePoster}
+          style={{ display: display }}
+          className="asciinema_poster"
+          src={`https://asciinema.org/a/${id}.svg`}
+        />
+      </div>
+      <iframe
+        className="asciinema_video"
+        allowfullscreen="true"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        src={`https://asciinema.org/a/${id}/iframe`}
+      ></iframe>
+    </div>
+  );
+}
+
+// <Asciinema id="296170" />
