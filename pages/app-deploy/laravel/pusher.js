5599059df6b7416d86581d727de47ac5017f48f8
userdehghani <m.a.dehghani609@gmail.com>
2021-12-01 13:26:06 +0330

    fix

--- pages/app-deploy/laravel/pusher.js
+++ pages/app-deploy/laravel/pusher.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../../components/Layout';
-import Highlight from 'react-highlight';
-import Notice from '../../../components/Notice';
 
 export default () => (
     <Layout>
