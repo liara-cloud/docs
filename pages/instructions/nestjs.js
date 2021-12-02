08a435524d0ee359f0d221e3a16b19530532ce90
userdehghani <m.a.dehghani609@gmail.com>
2021-12-02 18:53:45 +0330

    docs(nestjs): Add CORS

--- pages/instructions/nestjs.js
+++ pages/instructions/nestjs.js
@@ -1,13 +1,15 @@
-import Layout from '../../components/Layout';
-import Notice from '../../components/Notice';
-import Head from 'next/head';
-import Link from 'next/link';
-import Highlight from 'react-highlight';
+import Layout from "../../components/Layout";
+import Notice from "../../components/Notice";
+import Head from "next/head";
+import Link from "next/link";
+import Highlight from "react-highlight";
 
 export default () => (
     <Layout>
         <Head>
-            <title>استقرار برنامه‌های NestJS - سرویس ابری لیارا</title>
+            <title>
+                استقرار برنامه‌های NestJS - سرویس ابری لیارا
+            </title>
         </Head>
 
         <div className="page-head">
@@ -22,17 +24,10 @@ export default () => (
             </div>
         </div>
         <p>
-            NestJS یک فریم‌ورک قابل اعتماد و مقیاس‌پذیر با NodeJS است که بسیاری
-            از قابلیت‌ها را برای شما به ارمغان می‌آورد و شما می‌توانید
-            برنامه‌های NestJS خود را با ایجاد برنامه‌های{' '}
-            <Link href="/app-deploy/nodejs/getting-started">NodeJS</Link> بر روی
-            لیارا دیپلوی کنید.
+            NestJS یک فریم‌ورک قابل اعتماد و مقیاس‌پذیر با NodeJS است که بسیاری از قابلیت‌ها را برای شما به ارمغان می‌آورد و شما می‌توانید برنامه‌های NestJS خود را با ایجاد برنامه‌های <Link href="/app-deploy/nodejs/getting-started">NodeJS</Link> بر روی لیارا دیپلوی کنید.
         </p>
         <p>
-            توجه داشته باشید که برای دیپلوی برنامه‌های NestJS باید در بخش{' '}
-            <span className="code">scripts</span> فایل{' '}
-            <span className="code">package.json</span> مقدار{' '}
-            <span className="code">start</span> را به شکل زیر تغییر دهید:
+            توجه داشته باشید که برای دیپلوی برنامه‌های NestJS باید در بخش <span className="code">scripts</span> فایل <span className="code">package.json</span> مقدار <span className="code">start</span> را به شکل زیر تغییر دهید:
         </p>
         <Highlight className="json">{`"scripts": {
     "start": "node dist/main"
@@ -41,8 +36,9 @@ export default () => (
         <p>
             همچنین، توجه داشته باشید که لیارا به صورت خودکار دستور
             <span className="code">npm run build</span>
-            را اجرا می‌کند و نیازی نیست که تغییر خاصی برای اجرای این دستور اعمال
-            کنید. در نهایت دستور
+            را اجرا می‌کند و نیازی نیست که تغییر خاصی برای اجرای این دستور اعمال کنید.
+            
+            در نهایت دستور
             <span className="code">liara deploy --port 3000</span>
             را اجرا کنید تا برنامه‌ی شما به لیارا منتقل شده و اجرا شود.
         </p>
@@ -55,6 +51,6 @@ export default () => (
         <Highlight className="javascript">{`app.enableCors({
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-});`}</Highlight>   
+});`}</Highlight>
     </Layout>
 );
