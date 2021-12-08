639b80182c83e90885ebb49ea20a51c19a9240e0
userdehghani <m.a.dehghani609@gmail.com>
2021-12-08 10:54:13 +0330

    docs(docker): Improve

--- pages/app-deploy/docker/envs.js
+++ pages/app-deploy/docker/envs.js
@@ -1,15 +1,15 @@
-import Layout from "../../../components/Layout";
-import Link from "next/link";
-import Head from "next/head";
-import ZoomableImage from "../../../components/ZoomableImage";
-import Highlight from "react-highlight";
+import Layout from '../../../components/Layout';
+import Link from 'next/link';
+import Head from 'next/head';
+import ZoomableImage from '../../../components/ZoomableImage';
+import Highlight from 'react-highlight';
 
 export default () => (
   <Layout>
     <Head>
       <title>
-        مستندات تنظیم متغیرها (Environment Variables) در برنامه‌های Docker -
-        سرویس ابری لیارا
+        مستندات تنظیم متغیرها (Environment Variables) در برنامه‌های
+        Docker - سرویس ابری لیارا
       </title>
     </Head>
 
@@ -27,24 +27,32 @@ export default () => (
 
     <h3>تنظیم متغیرها (Environment Variables)</h3>
     <p>
-      متغیرهای محیطی یا همان Environment Variables به شما کمک می‌کنند تا
-      برنامه‌ی‌تان در هر محیط اطلاعات مربوط به همان محیط را دریافت کند. برای
-      مثال، اطلاعات اتصال به دیتابیس را می‌توانید از این بخش وارد کنید.{" "}
-      <Link href="/app-features/environment-variables">توضیحات بیشتر</Link>
+      متغیرهای محیطی یا همان Environment Variables کمک می‌کنند تا برنامه‌ی
+      شما در هر محیط، اطلاعات مربوط به همان محیط را دریافت کند.{' '}
+      <Link href="/app-features/environment-variables">
+        توضیحات بیشتر
+      </Link>
     </p>
+    <h4>تنظیم متغیرهای محیطی با لیارا CLI</h4>
     <p>
-      برای افزودن یک ENV کافیست که به بخش تنظیمات برنامه‌ی‌تان بروید و یک key به
-      عنوان اسم و یک value به عنوان مقدار اضافه کنید، و با زدن دکمه{" "}
-      <b>ثبت تغییرات</b> یک ENV به برنامه‌ی‌تان اضافه کنید. برای نمونه، ENV ای
-      با عنوان LIARA_URL و مقدار https://liara.ir را به این صورت اضافه کنید.
+      برای افزودن یک متغیر محیطی باعنوان{' '}
+      <span className="code">LIARA_URL</span> و مقدار https://liara.ir
+      می‌توانید از دستور <Link href="/cli/env">liara env</Link> استفاده
+      کرده و به‌شکل زیر متغیر مورد نظرتان را تنظیم کنید:
+    </p>
+    <Highlight className="bash">{`$ liara env:set LIARA_URL=https://liara.ir`}</Highlight>
+    <h4>تنظیم متغیرهای محیطی در پنل کاربری</h4>
+    <p>
+      برای افزودن یک متغیر محیطی باید وارد تنظیمات برنامه شده و یک KEY به
+      عنوان اسم و یک value به عنوان مقدار اضافه کنید و با کلیک بر روی
+      دکمه‌ی <strong>ثبت تغییرات</strong>، یک متغیر محیطی به برنامه‌ی شما
+      اضافه خواهد شد.
     </p>
     <ZoomableImage src="/static/php-env.png" />
-    <p> حالا به راحتی می‌توانید از‌ آن در برنامه‌ی‌تان استفاده کنید.</p>
     <p>
-      بعد از کلیک روی دکمه <b>ثبت تغییرات</b> برنامه‌ی‌ شما به صورت خودکار
-      ری‌استارت می‌شود و در استقرار جدید این متغیر‌های محیطی در داخل برنامه قابل
-      استفاده می‌شوند. به این شیوه که LIARA_URL را اضافه کردید می‌توانید هر ENV
-      دیگری را نیز به برنامه اضافه کنید.
+      پس از کلیک بر روی دکمه <strong>ثبت تغییرات</strong>، برنامه‌ی‌ شما
+      به صورت خودکار ری‌استارت می‌شود و این متغیر محیطی در استقرار جدید
+      قابل استفاده خواهد بود.
     </p>
     <Link href="/app-deploy/docker/logs">متوجه شدم، برو گام بعدی!</Link>
   </Layout>
--- pages/app-deploy/docker/tips.js
+++ pages/app-deploy/docker/tips.js
@@ -1,8 +1,8 @@
-import Layout from "../../../components/Layout";
-import Notice from "../../../components/Notice";
-import Head from "next/head";
-import Link from "next/link";
-import Highlight from "react-highlight";
+import Layout from '../../../components/Layout';
+import Notice from '../../../components/Notice';
+import Head from 'next/head';
+import Link from 'next/link';
+import Highlight from 'react-highlight';
 
 export default () => (
     <Layout>
@@ -27,53 +27,49 @@ export default () => (
         <h3> توضیحات و نکات تکمیلی</h3>
 
         <h3 id="limits">محدودیت‌ها</h3>
+        <h4>
+            {' '}
+            استفاده از <span className="code">VOLUME</span>
+            در Dockerfile
+        </h4>
         <p>
-            <ol>
-                <li>
-                    استفاده از <span className="code">VOLUME</span>
-                    در Dockerfile:
-                    فایل‌سیستم لیارا ReadOnly
-                    است و شما نمی‌تونید از دستور
-                    <span className="code">VOLUME</span>
-                    در Dockerfile
-                    استفاده کنید و به‌جای آن باید از قابلیت «دیسک‌ها» استفاده کنید. لازم است که این عبارت را قبل از استقرار حذف کنید.
-                    چنانچه از Image‌های
-                    DockerHub هم استفاده کنید، باید نظیر به‌نظیر
-                    <span className="code">VOLUME</span>
-                    هایی که در آن Image
-                    تعریف شده، دیسک بسازید و دیسک را در همان مسیر mount
-                    کنید.
-                    <br />
-                    برای مثال، Dockerfile زیر را در نظر بگیرید:
-                    <pre>
-                        <code>
-                            {`FROM ubuntu
+            <Link href="/app-features/file-system">
+                فایل سیستم برنامه‌های لیارا
+            </Link>{' '}
+            Read-Only است و شما نمی‌تونید از دستور
+            <span className="code">VOLUME</span>
+            در Dockerfile استفاده کنید و به‌جای آن باید از قابلیت «
+            <Link href="/app-deploy/docker/disks">دیسک‌ها</Link>» استفاده کنید.
+            لازم است که این عبارت را قبل از استقرار حذف کنید. چنانچه از{' '}
+            <Link href="/app-deploy/docker/deploy-image">
+                Image‌های DockerHub
+            </Link>{' '}
+            هم استفاده کنید، باید نظیر به‌نظیر
+            <span className="code">VOLUME</span>
+            هایی که در آن Image تعریف شده، دیسک بسازید و دیسک را در همان مسیر
+            mount کنید.
+            <br />
+            برای مثال، Dockerfile زیر را در نظر بگیرید:
+            <Highlight className="dockerfile">
+                {`FROM ubuntu
 
 RUN echo hello
 
 VOLUME /path/to/data`}
-                        </code>
-                    </pre>
-                    این فایل برای این‌که بتواند در لیارا مستقر شود، باید خطی که در آن
-                    <span className="code">VOLUME</span>
-                    تعریف شده، حذف شود.
-                </li>
-                <li>
-                    استفاده از <span className="code">EXPOSE</span>
-                    در Dockerfile:
-                    لیارا از این قسمت از Dockerfile
-                    شما صرف‌نظر می‌کند. شما باید حتما پورتی که مدنظرتان است را با پارامتر
-                    <span className="code">--port</span>
-                    تنظیم کنید.
-                </li>
-                <li>
-                    تنها به‌پورت‌های HTTP
-                    می‌توانید از خارج از شبکه‌ی لیارا متصل شوید. برای مثال اگر یک سرویس مانند
-                    RabbitMQ
-                    را مستقر کنید، به‌این سرویس تنها در شبکه‌ی خصوصی لیارا دسترسی خواهید داشت.
-                </li>
-            </ol>
+            </Highlight>
+            این فایل برای این‌که بتواند در لیارا مستقر شود، باید خطی که در آن
+            <span className="code">VOLUME</span>
+            تعریف شده، حذف شود.
+            <h4>
+                استفاده از <span className="code">EXPOSE</span>
+                در Dockerfile
+            </h4>
+            لیارا از این قسمت از Dockerfile شما صرف‌نظر می‌کند. شما باید حتما
+            پورتی که مدنظرتان است را با پارامتر
+            <span className="code">--port</span>
+            تنظیم کنید. تنها به‌پورت‌های HTTP می‌توانید از خارج از شبکه‌ی لیارا
+            متصل شوید. برای مثال اگر یک سرویس مانند RabbitMQ را مستقر کنید،
+            به‌این سرویس تنها در شبکه‌ی خصوصی لیارا دسترسی خواهید داشت.
         </p>
-
     </Layout>
 );
