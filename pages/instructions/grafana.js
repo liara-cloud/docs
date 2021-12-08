27aab50d008de532cc83342572ddbfe138a4a92d
userdehghani <m.a.dehghani609@gmail.com>
2021-12-08 16:16:57 +0330

    docs(grafana): Deploy grafana with liara cli

--- pages/instructions/grafana.js
+++ pages/instructions/grafana.js
@@ -1,14 +1,13 @@
-import Layout from "../../components/Layout"
-import Head from "next/head"
-import Link from "next/link"
-import Highlight from "react-highlight"
+import Head from 'next/head';
+import Link from 'next/link';
+import Highlight from 'react-highlight';
+import Layout from '../../components/Layout';
+import Asciinema from '../../components/Asciinema';
 
 export default () => (
     <Layout>
         <Head>
-            <title>
-                استقرار Grafana - سرویس ابری لیارا
-            </title>
+            <title>استقرار Grafana - سرویس ابری لیارا</title>
         </Head>
 
         <div className="page-head">
@@ -24,20 +23,25 @@ export default () => (
         </div>
 
         <p>
-            گرافانا پلتفرمی بسیار محبوب و ساده برای تحلیل و نمایش داده‌های به دست آمده از پایگاه‌های داده خصوصا
-            دیتابیس‌های سری زمانی (Time-series Databases) است. گرافانا از دیتابیس‌های مختلفی از جمله InfluxDB و
+            گرافانا پلتفرمی بسیار محبوب و ساده برای تحلیل و نمایش داده‌های به
+            دست آمده از پایگاه‌های داده خصوصا دیتابیس‌های سری زمانی (Time-series
+            Databases) است. گرافانا از دیتابیس‌های مختلفی از جمله InfluxDB و
             Prometheus پشتیبانی می‌کند.
         </p>
 
         <p>
-            برای استقرار این برنامه، ابتدا لازم است که از بخش «برنامه‌ها» یک برنامه از نوع <Link
-                href="/app-deploy/docker/getting-started">Docker</Link> با نام و پلن دلخواه‌تان بسازید.
+            برای استقرار این برنامه، ابتدا لازم است که از بخش «برنامه‌ها» یک
+            برنامه از نوع{' '}
+            <Link href="/app-deploy/docker/getting-started">Docker</Link> با نام
+            و پلن دلخواه‌تان بسازید.
             <br />
-            سپس یک دیسک طبق مستندات «<Link href="/app-deploy/docker/disks">استفاده از دیسک‌ها</Link>» بسازید.
+            سپس یک دیسک طبق مستندات «
+            <Link href="/app-deploy/docker/disks">استفاده از دیسک‌ها</Link>»
+            بسازید.
         </p>
         <p>
-            در مرحله بعد یک فایل<span className="code"> liara.json</span> طبق راهنمایی زیر بسازید و مشخصات مربوطه را در
-            این فایل وارد نمایید.
+            در مرحله بعد یک فایل<span className="code"> liara.json</span> طبق
+            راهنمایی زیر بسازید و مشخصات مربوطه را در این فایل وارد نمایید.
             <Highlight>
                 {`{
   "image": "grafana/grafana:[نسخه مورد نظر]",
@@ -53,37 +57,54 @@ export default () => (
             </Highlight>
         </p>
         <p>
-            در نهایت، CMD و یا ترمینال
-            را در پوشه‌ای که <span className="code">liara.json</span>
-            را داخل آن قرار دادید باز کرده و سپس
-            دستور زیر را برای استقرار و اجرای برنامه وارد کنید:
+            در نهایت، CMD و یا ترمینال را در پوشه‌ای که{' '}
+            <span className="code">liara.json</span>
+            را داخل آن قرار دادید باز کرده و سپس دستور زیر را برای استقرار و
+            اجرای برنامه وارد کنید:
         </p>
         <pre>
             <code>$ liara deploy</code>
         </pre>
         <p>
-            <Link href="/cli/install">
-                راهنمای نصب Liara CLI
-            </Link>
+            <Link href="/cli/install">راهنمای نصب Liara CLI</Link>
         </p>
         <h3>توجه داشته باشید که</h3>
         <ul>
             <li>
-                بین برنامه‌ها و دیتابیس‌ها شبکه‌ی خصوصی برقرار است که در صورت استقرار
-                میکروسرویس‌ها، ارتباط درون‌شبکه‌ای و استفاده از Grafana، بسیار کاربردی است.
+                بین برنامه‌ها و دیتابیس‌ها شبکه‌ی خصوصی برقرار است که در صورت
+                استقرار میکروسرویس‌ها، ارتباط درون‌شبکه‌ای و استفاده از Grafana،
+                بسیار کاربردی است.
             </li>
             <li>
-                در صفحه‌ی <Link href="/app-features/logs">لاگ‌ها</Link> امکان دنبال‌کردن زنده‌ی
-                لاگ‌های‌تان را دارید.
+                در صفحه‌ی <Link href="/app-features/logs">لاگ‌ها</Link> امکان
+                دنبال‌کردن زنده‌ی لاگ‌های‌تان را دارید.
             </li>
             <li>
-                بهتر است برای تعیین نسخه از <span className="code">latest</span> استفاده نکنید بلکه به صورت صریح شماره
-                نسخه مورد نظر را وارد نمایید.
+                بهتر است برای تعیین نسخه از <span className="code">latest</span>{' '}
+                استفاده نکنید بلکه به صورت صریح شماره نسخه مورد نظر را وارد
+                نمایید.
             </li>
             <li>
-                برای اطلاع از تنظیمات بیشتر و نسخه‌های مختلف می‌توانید از مستندات مربوطه در <Link
-                    href="https://hub.docker.com/r/grafana/grafana">Grafana</Link> استفاده کنید.
+                برای اطلاع از تنظیمات بیشتر و نسخه‌های مختلف می‌توانید از
+                مستندات مربوطه در{' '}
+                <Link href="https://hub.docker.com/r/grafana/grafana">
+                    Grafana
+                </Link>{' '}
+                استفاده کنید.
             </li>
         </ul>
+        <h3>استقرار سریع</h3>
+        <p>
+            همچنین شما می‌توانید تمام مراحل فوق را با استفاده از{' '}
+            <Link href="/cli/install">لیارا CLI</Link> انجام دهید:
+        </p>
+        <Highlight className="bash">
+            {`$ liara deploy --app grafana-platform \\
+               --image grafana/grafana:8.0.0 \\
+               --port 3000 \\
+               --disks grafana:/var/lib/grafana \\
+               --detach`}
+        </Highlight>
+        <Asciinema id="454560" />
     </Layout>
 );
