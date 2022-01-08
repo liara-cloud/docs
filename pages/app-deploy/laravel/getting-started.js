97ad65797070619b13a320b0c0e121c07bed49c7
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-01-08 12:33:27 +0330

    docs(laravel-lumen): Update supported versions (#166)

--- pages/app-deploy/laravel/getting-started.js
+++ pages/app-deploy/laravel/getting-started.js
@@ -47,10 +47,10 @@ export default () => (
     </p>
 
     <ul dir="ltr">
-      <li>Laravel 5.5.^</li>
-      <li>Laravel 6.^</li>
-      <li>Laravel 7.^</li>
-      <li>Laravel 8.^</li>
+      <li>Laravel and Lumen 5.5.^</li>
+      <li>Laravel and Lumen 6.^</li>
+      <li>Laravel and Lumen 7.^</li>
+      <li>Laravel and Lumen 8.^</li>
     </ul>
 
     <p>
