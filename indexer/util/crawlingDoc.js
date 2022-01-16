f17656a14329579614dedeb46f61ade8cd3af865
MohammadReza <mrvashian@gmail.com>
2022-01-16 09:45:44 +0330

    Update crawlingDoc.js

--- indexer/util/crawlingDoc.js
+++ indexer/util/crawlingDoc.js
@@ -1,49 +1,60 @@
-import { writeFile } from "fs/promises";
+import { writeFile } from 'fs/promises';
 
-import { load } from "cheerio";
-import got from "got";
-import { v4 as uuidv4 } from "uuid";
+import { load } from 'cheerio';
+import got from 'got';
+import { v4 as uuidv4 } from 'uuid';
 
 // data
 const data = [];
 const links = [];
-const url = "https://docs.liara.ir/";
+const url = 'https://docs.liara.ir/';
 // collect data
 export async function crawlingDocData() {
   // Get all link
   const { body } = await got.get(url);
   const $ = load(body);
-  $("a.nav__link").each(function (i, el) {
-    links.push($(this).attr("href"));
+  $('a.nav__link').each(function (i, el) {
+    links.push($(this).attr('href'));
   });
 
   await Promise.all(
-    links.map(async link => {
+    links.map(async (link) => {
       const { body } = await got.get(url + link);
       const $ = load(body);
-      if (!$("h4").text()) {
-        $("article > h3,h4").each(function (i, el) {
-          const platform = $("article .page-icon").attr("alt");
-          data.push({
-            id: uuidv4(),
-            url: link,
-            platform,
-            title: $(this).text(),
-            body: $(this).nextUntil("h3").text(),
-          });
-        });
-      }
-      if ($("h4").next().is("ul")) {
-        $("article > *").each(function (i, el) {
-          if (typeof $(this).attr("id") != "undefined") {
-            const platform = $("article .page-icon").attr("alt");
+      if (!$('h4').text()) {
+        const platform = $('article .page-icon').attr('alt');
+        if ($('article > h3').length) {
+          $('article > h3,h4').each(function (i, el) {
             data.push({
               id: uuidv4(),
               url: link,
               platform,
-              element: `#${$(this).attr("id")}`,
               title: $(this).text(),
-              body: $(this).nextUntil("h3").text(),
+              body: $(this).nextUntil('h3').text(),
+            });
+          });
+        } else {
+          data.push({
+            id: uuidv4(),
+            url: link,
+            platform,
+            title:
+              $('article .page-title > h1').text() || $('article > h1').text(),
+            body: $('article').text(),
+          });
+        }
+      }
+      if ($('h4').next().is('ul')) {
+        $('article > *').each(function (i, el) {
+          if (typeof $(this).attr('id') != 'undefined') {
+            const platform = $('article .page-icon').attr('alt');
+            data.push({
+              id: uuidv4(),
+              url: link,
+              platform,
+              element: `#${$(this).attr('id')}`,
+              title: $(this).text(),
+              body: $(this).nextUntil('h3').text(),
             });
           }
         });
@@ -51,6 +62,6 @@ export async function crawlingDocData() {
     })
   );
 
-  await writeFile("data/docData.json", JSON.stringify(data));
+  await writeFile('data/docData.json', JSON.stringify(data));
   return data;
 }
