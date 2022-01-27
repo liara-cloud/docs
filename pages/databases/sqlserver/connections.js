70ec222505cfd243a7422298b78a11cca58338cf
userdehghani <m.a.dehghani609@gmail.com>
2022-01-27 16:13:54 +0330

    Improve sql-server connection section

--- pages/databases/sqlserver/connections.js
+++ pages/databases/sqlserver/connections.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Highlight from "react-highlight";
 import Layout from "../../../components/Layout";
 import PlatformIcon from "../../../components/PlatformIcon";
 import ZoomableImage from "../../../components/ZoomableImage";
@@ -19,59 +20,51 @@ export default () => (
 
     <h3>راه‌های اتصال به SQL Server</h3>
     <p>
-      در صورتی که قصد دارید داده‌های دیتابیس‌‌تان را ببینید و در یک محیط گرافیکی
-      یا کامندلاینی به مدیریت دیتابیس‌‌تان بپردازید، می‌توانید از راه‌های زیر
-      اقدام کنید:
+      پس از نصب و راه‌اندازی یک دیتابیس جدید نوبت به مدیریت آن می‌رسد و شما
+      می‌توانید برای مشاهده و مدیریت داده‌های دیتابیس‌های SQL Server یکی از
+      ابزارهای زیر را انتخاب کنید.
     </p>
-    <ul>
-      <li>
-        <b>استفاده از Azure Data Studio</b>
-      </li>
-      <p>
-        بعد از نصب{" "}
-        <a href="https://github.com/microsoft/azuredatastudio" target="_blank">
-          Azure Data Studio
-        </a>{" "}
-        کافیست با دادن اطلاعات دیتابیس connect شوید. توجه داشته باشید که در فیلد
-        Server پورت را با علامت "," جدا کرده‌ایم. یعنی:{" "}
-        <span className="code">s11.liara.ir,34472</span>
-      </p>
-      <ZoomableImage src="/static/databases/sqlserver-azurestudio.png" />
+    <h4 id="azure-data-studio">Azure Data Studio</h4>
+    <p>
+      اگر بخواهید سرویس دیتابیس را در محیط سیستم‌عامل مدیریت کنید می‌توانید از
+      ابزار{" "}
+      <a href="https://github.com/microsoft/azuredatastudio" target="_blank">
+        Azure Data Studio
+      </a>{" "}
+      استفاده کرده و با وارد کردن اطلاعات اتصال، به سرویس دیتابیس متصل شوید.
+      توجه داشته باشید که در فیلد Server پورت را با علامت{" "}
+      <span className="code">,</span> جدا کرده‌ایم. یعنی:{" "}
+      <span className="code">s11.liara.ir,34472</span>
+    </p>
+    <ZoomableImage src="/static/databases/sqlserver-azurestudio.png" />
 
-      <br />
-      <li>
-        <b>استفاده از sqlcmd</b>
-      </li>
-      <p>
-        بعد از نصب{" "}
-        <a
-          href="https://docs.microsoft.com/en-us/sql/tools/sqlcmd-utility?view=sql-server-ver15"
-          target="_blank"
-        >
-          sqlcmd
-        </a>{" "}
-        کافیست برای اتصال دستور زیر را وارد کنید:
-      </p>
-      <code>{`$ sqlcmd -S DB_URL,DB_PORT -Usa -P DB_PASSWORD`}</code>
-      <br />
-      <ZoomableImage src="/static/databases/sqlcmd.png" />
-      <br />
-      <li>
-        <p>
-          <b>
-            {" "}
-            با مراجعه به این بخش از{" "}
-            <a
-              href="https://docs.microsoft.com/en-us/sql/tools/overview-sql-tools?view=sql-server-ver15"
-              target="_blank"
-            >
-              مستندات Microsoft
-            </a>{" "}
-            می‌توانید ابزارهای بیشتری را برای اتصال به دیتابیس SQL Server
-            ببینید.
-          </b>
-        </p>
-      </li>
-    </ul>
+    <h4 id="sqlcmd">sqlcmd</h4>
+    <p>
+      در صورتی که قصد داشته باشید از طریق خط فرمان سیستم‌عامل خود به دیتابیس‌های
+      SQL Server متصل شوید می‌توانید ابزار{" "}
+      <a
+        href="https://docs.microsoft.com/en-us/sql/tools/sqlcmd-utility?view=sql-server-ver15"
+        target="_blank"
+      >
+        sqlcmd
+      </a>{" "}
+      را نصب کرده و با اجرای دستور زیر به سرویس دیتابیس خود متصل شوید.
+    </p>
+    <Highlight className="bash">{`$ sqlcmd -S DB_URL,DB_PORT -Usa -P DB_PASSWORD`}</Highlight>
+
+    <br />
+
+    <ZoomableImage src="/static/databases/sqlcmd.png" />
+    <p>
+      همچنین شما می‌توانید با مراجعه به این بخش از{" "}
+      <a
+        href="https://docs.microsoft.com/en-us/sql/tools/overview-sql-tools?view=sql-server-ver15"
+        target="_blank"
+      >
+        مستندات Microsoft
+      </a>
+      ، به لیست کامل‌تری از ابزارهای اتصال به دیتابیس‌های SQL Server دست پیدا
+      کنید.
+    </p>
   </Layout>
 );
