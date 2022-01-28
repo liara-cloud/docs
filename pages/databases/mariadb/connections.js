a782f0b3ded3146b7e0392777bffff99b3710af3
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-01-28 16:43:59 +0330

    Improve sql-database connection section (#213)

--- pages/databases/mariadb/connections.js
+++ pages/databases/mariadb/connections.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Notice from "../../../components/Notice";
+import Highlight from "react-highlight";
 import Layout from "../../../components/Layout";
 import PlatformIcon from "../../../components/PlatformIcon";
 import ZoomableImage from "../../../components/ZoomableImage";
@@ -20,66 +20,74 @@ export default () => (
 
     <h3>راه‌های اتصال به MariaDB</h3>
     <p>
-      در صورتی که قصد دارید داده‌های دیتابیس‌‌تان را ببینید و در یک محیط گرافیکی
-      یا کامندلاینی به مدیریت دیتابیس‌‌تان بپردازید، می‌توانید از راه‌های زیر
-      اقدام کنید:
+      پس از نصب و راه‌اندازی یک دیتابیس جدید نوبت به مدیریت آن می‌رسد و شما
+      می‌توانید برای مشاهده و مدیریت داده‌های دیتابیس‌های MariaDB یکی از
+      ابزارهای زیر را انتخاب کنید.
     </p>
-    <ul>
-      <li>
-        <b>استفاده از PHPMyAdmin</b>
-      </li>
-      <p>
-        لیارا برای دیتابیس MySQL / MariaDB امکان راه‌اندازی PHPMyAdmin را به
-        راحتی فراهم کرده است. برای این کار کافیست که به صفحه دیتابیس مورد نظرتان
-        بروید و از بخش <b>راه‌اندازی PHPMyAdmin</b> آن را اجرا کنید.
-      </p>
-      <ZoomableImage
-        src="/static/databases/mysql-create-4.png"
-        alt="آماده شدن دیتابیس"
-      />
-      <p>
-        سپس با نام کاربری و رمز عبور دیتابیس‌تان وارد PHPMyAdmin شوید و از
-        امکانات آن استفاده کنید:
-      </p>
-      <ZoomableImage
-        src="/static/databases/mysql-phpmyadmin.png"
-        alt="پنل phpmyadmin"
-      />
-      <br />
-      <li>
-        <b>استفاده از MySQL Workbench</b>
-      </li>
-      <p>
-        اگر از نرم‌افزار Workbench روی سیستم‌تان استفاده می‌کنید، می‌توانید به
-        وسیله‌ی آن به دیتابیس‌‌تان در لیارا متصل شوید. کافیست اطلاعات اتصال به
-        دیتابیس‌‌تان را وارد کنید. بعد از وارد کردن اطلاعات کافی‌ست تست اتصال یا
-        همان Test Connection را بزنید تا مطمئن شوید اتصال به لیارا با موفقیت
-        برقرار شده است.
-      </p>
-      <ZoomableImage
-        src="/static/databases/mysql-workbrench.png"
-        alt="پنل phpmyadmin"
-      />
-      <br />
-      <li>
-        <b>استفاده از MySQL CLI</b>
-      </li>
-      <p>
-        در صورتی که قصد دارید از طریق MySQL CLI به دیتابیس‌‌تان متصل شوید کافیست
-        که ابتدا mysql-client را متناسب با سیستم‌عامل‌تان نصب کنید. سپس به روش
-        زیر می‌توانید به دیتابیس متصل شوید:
-      </p>
-      <code>
-        {`$ mysql -u DB_USER -pDB_PASSWORD --host DB_HOST --port DB_PORT`}
-      </code>
-      <br />
+    <h4 id="phpmyadmin">PHPMyAdmin</h4>
+    <p>
+      اکثر توسعه‌دهندگان با ابزار مشهور PHPMyAdmin که امکان مدیریت دیتابیس‌های
+      MySQL و MariaDB را در محیط وب فراهم می‌کند، آشنا هستند. برای راه‌اندازی
+      این ابزار در لیارا تنها کافیست وارد منوی <strong>نحوه‌ی اتصال</strong> به
+      دیتابیس شده و گزینه‌ی <strong>راه‌اندازی PHPMyAdmin</strong> را فعال کنید.
+    </p>
+    <ZoomableImage
+      src="/static/databases/mysql-create-4.png"
+      alt="آماده شدن دیتابیس"
+    />
+    <p>
+      سپس می‌توانید با کلیک بر روی دکمه‌ی <strong>باز کردن PHPMyAdmin</strong> و
+      وارد کردن نام کاربری و رمز عبور دیتابیس‌تان از امکانات این ابزار استفاده
+      کنید.
+    </p>
+    <ZoomableImage
+      src="/static/databases/mysql-phpmyadmin.png"
+      alt="پنل phpmyadmin"
+    />
 
-      <ZoomableImage src="/static/databases/mysql-cli.png" alt="mysql cli" />
-    </ul>
-    <Notice variant="info">
-      شما می‌توانید از هر نرم‌افزار دلخواهی به MySQL در لیارا متصل شوید. در
-      صورتی که مشکلی در اتصال به دیتابیس MySQL داشتید، از طریق تیکت به پشتیبانی
-      لیارا اطلاع دهید.
-    </Notice>
+    <h4 id="mysql-workbench">MySQL Workbench</h4>
+    <p>
+      حال اگر بخواهید سرویس دیتابیس را در محیط سیستم‌عامل مدیریت کنید می‌توانید
+      یکی از ابزارهای{" "}
+      <a
+        href="https://www.mysql.com/products/workbench/"
+        target="_blank"
+        rel="noopener"
+      >
+        MySQL Workbench
+      </a>
+      ،{" "}
+      <a href="https://dbeaver.io/" target="_blank" rel="noopener">
+        DBeaver
+      </a>{" "}
+      یا{" "}
+      <a href="https://www.heidisql.com/" target="_blank" rel="noopener">
+        HeidiSQL
+      </a>{" "}
+      را انتخاب کنید. اتصال به دیتابیس با استفاده از این ابزارها بسیار ساده است
+      و برای مثال در ابزار MySQL Workbench پس از وارد کردن اطلاعات اتصال به
+      دیتابیس می‌توانید روی دکمه‌ی <strong>Test Connection</strong> کلیک کرده و
+      به این شکل از صحت اطلاعات وارد شده اطمینان حاصل کنید. درنهایت می‌توانید
+      روی گزینه‌ی <strong>OK</strong> کلیک کنید تا امکان مدیریت داده‌های سرویس
+      دیتابیس و استفاده از امکانات این ابزار برای شما فراهم شود.
+    </p>
+    <ZoomableImage
+      src="/static/databases/mysql-workbrench.png"
+      alt="پنل phpmyadmin"
+    />
+    <h4 id="mysql-client">MySQL Command-Line Client</h4>
+    <p>
+      در صورتی که قصد داشته باشید از طریق خط فرمان سیستم‌عامل خود به دیتابیس‌های
+      MySQL و MariaDB متصل شوید می‌توانید ابزار MySQL Command-Line Client را نصب
+      کرده و با اجرای دستور زیر به سرویس دیتابیس خود متصل شوید.
+    </p>
+
+    <Highlight className="bash">
+      {`$ mysql -u DB_USER -pDB_PASSWORD --host DB_HOST --port DB_PORT`}
+    </Highlight>
+
+    <br />
+
+    <ZoomableImage src="/static/databases/mysql-cli.png" alt="mysql cli" />
   </Layout>
 );
