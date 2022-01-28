c56299a5b5a02aad5a58d64d9197d6a4c515ed01
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-01-28 16:54:25 +0330

    Improve mongodb database connection section (#216)

--- pages/databases/mongodb/connections.js
+++ pages/databases/mongodb/connections.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Highlight from "react-highlight";
 import Layout from "../../../components/Layout";
 import PlatformIcon from "../../../components/PlatformIcon";
 import ZoomableImage from "../../../components/ZoomableImage";
@@ -19,81 +20,71 @@ export default () => (
 
     <h3>راه‌های اتصال به MongoDB</h3>
     <p>
-      در صورتی که قصد دارید داده‌های دیتابیس‌‌تان را ببینید و در یک محیط گرافیکی
-      یا کامندلاینی به مدیریت دیتابیس‌‌تان بپردازید، می‌توانید از راه‌های زیر
-      اقدام کنید:
+      پس از نصب و راه‌اندازی یک دیتابیس جدید نوبت به مدیریت آن می‌رسد و شما
+      می‌توانید برای مشاهده و مدیریت داده‌های دیتابیس‌های MongoDB یکی از
+      ابزارهای زیر را انتخاب کنید.
     </p>
-    <ul>
-      <li>
-        <b>استفاده از نرم‌افزار محبوب Robo 3T</b>
-      </li>
+
+    <h4 id="robo-3t">Robo 3T</h4>
+    <p>
+      اگر بخواهید دیتابیس‌های MongoDB را در محیط سیستم‌عامل مدیریت کنید
+      می‌توانید از ابزار{" "}
+      <a href="https://robomongo.org/" target="_blank" rel="noopenner">
+        Robo 3T
+      </a>{" "}
+      که اکثر توسعه‌دهندگان آن را با نام سابقش یعنی RoboMongo می‌شناسند، استفاده
+      کنید. این ابزار رایگان و متن‌باز در تمامی سیستم‌عامل‌ها قابل اجرا هست و
+      شما می‌توانید به‌سادگی و با دنبال کردن مراحل زیر، با استفاده از این ابزار
+      به سرویس دیتابیس خود متصل شوید.
       <p>
-        {" "}
-        <a target="_blank" href="https://robomongo.org/">
-          Robo 3T
-        </a>
-        ، که با نام سابق RoboMongo آشناتر است، یک محیط GUI که در تمام
-        سیستم‌عامل‌ها نیز پشتیبانی می‌شود در اختیارتان قرار می‌دهد که با آن
-        می‌توانید به دیتابیس‌های مونگودی‌بی متصل شده و داده‌های‌تان را مدیریت
-        کنید. این نرم‌افزار، رایگان و OpenSource است. بعد از نصب این نرم‌افزار
-        کافیست برای اتصال مراحل زیر را انجام دهید:
-        <ul>
-          <li>
-            <p>
-              در هنگام بالاآمدن نرم‌افزار روی New Connection کلیک کنید و اطلاعات
-              آن را متناسب با اطلاعات دیتابیس‌ خودتان تکمیل کنید.
-            </p>
-          </li>
-          <ZoomableImage src="/static/databases/robo3t-auth1.png" />
-          <p>
-            در tab مربوط به Authentication کافیست Mode را روی حالت SCRAM-SHA-1
-            قرار دهید و Database را با مقدار اولیه admin بدون تغییر خاصی باقی
-            بگذارید.
-          </p>
-          <ZoomableImage src="/static/databases/robo3t-auth2.png" />
-          <p>
-            سپس کافیست روی Test کلیک کنید تا مطمئن شوید همه چیز با موفقیت انجام
-            شده است.
-          </p>
-          <ZoomableImage src="/static/databases/robo3t-test.png" />
-        </ul>
+        در هنگام بالاآمدن نرم‌افزار روی New Connection کلیک کنید و اطلاعات آن را
+        متناسب با اطلاعات دیتابیس‌ خودتان تکمیل کنید.
       </p>
-      <li>
-        <b>استفاده از mongo cli</b>
-      </li>
+      <ZoomableImage src="/static/databases/robo3t-auth1.png" />
       <p>
-        برای اتصال با cli کافیست که mongo در سیستم شما نصب شده باشد و به کمک
-        ترمینال می‌توانید با استفاده از این دستور به دیتابیس‌‌تان در لیارا متصل
-        شوید:
+        در tab مربوط به Authentication کافیست Mode را روی حالت SCRAM-SHA-1 قرار
+        دهید و Database را با مقدار اولیه admin بدون تغییر خاصی باقی بگذارید.
       </p>
-      <code>
-        {`$ mongo -u DB_USERNAME -p DB_PASSWORD --host DB_HOST --port DB_PORT --authenticationDatabase admin`}
-      </code>
-      <br />
+      <ZoomableImage src="/static/databases/robo3t-auth2.png" />
+      <p>
+        سپس کافیست روی Test کلیک کنید تا مطمئن شوید همه چیز با موفقیت انجام شده
+        است.
+      </p>
+      <ZoomableImage src="/static/databases/robo3t-test.png" />
+    </p>
+    <h4>mongo cli</h4>
+    <p>
+      در صورتی که قصد داشته باشید از طریق خط فرمان سیستم‌عامل خود به دیتابیس‌های
+      MongoDB متصل شوید می‌توانید ابزار Mongo Shell را نصب کرده و با اجرای دستور
+      زیر به سرویس دیتابیس خود متصل شوید.
+    </p>
+    <Highlight className="bash">
+      {`$ mongo -u DB_USERNAME -p DB_PASSWORD --host DB_HOST --port DB_PORT --authenticationDatabase admin`}
+    </Highlight>
+    <br />
 
-      <ZoomableImage src="/static/databases/mongo-cli.png" />
+    <ZoomableImage src="/static/databases/mongo-cli.png" />
 
-      <br />
+    <h4>استفاده از پنل‌های رایگان و متن باز</h4>
 
-      <li>
-        <b>استفاده از پنل‌های رایگان و اپن سورس</b>
-      </li>
-      <br />
-      <ul>
-        <li>
-          <a
-            href="https://github.com/mongo-express/mongo-express"
-            target="_blank"
-          >
-            mongo-express
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/mrvautin/adminMongo" target="_blank">
-            adminMongo
-          </a>
-        </li>
-      </ul>
-    </ul>
+    <p>
+      همچنین شما می‌توانید از پنل‌های رایگان و متن‌باز مانند{" "}
+      <a
+        href="https://github.com/mongo-express/mongo-express"
+        rel="noopenner"
+        target="_blank"
+      >
+        mongo-express
+      </a>{" "}
+      و یا{" "}
+      <a
+        href="https://github.com/mrvautin/adminMongo"
+        rel="noopenner"
+        target="_blank"
+      >
+        adminMongo
+      </a>{" "}
+      استفاده کنید.
+    </p>
   </Layout>
 );
