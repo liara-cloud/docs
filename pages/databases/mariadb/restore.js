d872c1e6d6fff62544361b4c6cb68bed3afcd8e1
userdehghani <m.a.dehghani609@gmail.com>
2022-02-04 21:11:05 +0330

    Restore backup file into mariadb with phpmyadmin

--- pages/databases/mariadb/restore.js
+++ pages/databases/mariadb/restore.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Highlight from "react-highlight";
 import Layout from "../../../components/Layout";
+import Notice from "../../../components/Notice";
 import Asciinema from "../../../components/Asciinema";
 import PlatformIcon from "../../../components/PlatformIcon";
+import ZoomableImage from "../../../components/ZoomableImage";
 
 export default () => (
   <Layout>
@@ -23,10 +25,31 @@ export default () => (
     <h3>بازیابی فایل پشتیبان</h3>
 
     <p>
-      برای بازگردانی فایل پشتیبان در دیتابیس‌های MariaDB می‌توانید به‌شکل زیر از
-      ابزار <span className="code">mysql-client</span> استفاده کنید.
+      ممکن است در زمان انتقال سرویس و یا پس از مواجه شدن با هرگونه Data
+      corruption تصمیم بگیرید فایل پشتیبان دیتابیس خود را بازیابی کنید.
     </p>
 
+    <h4 id="phpmyadmin">phpMyAdmin</h4>
+    <p>
+      بازیابی فایل پشتیبان در دیتابیس‌های MySQL و MariaDB یکی از قابلیت‌هایی است
+      که ابزار مشهور phpMyAdmin در اختیار شما قرار می‌‌دهد و شما می‌توانید
+      به‌شکل زیر از این ابزار برای بازیابی فایل‌های پشتیبان استفاده کنید.
+    </p>
+
+    <ZoomableImage src="https://files.liara.ir/docs/mariadb/restore-backup-into-mariadb-database-with-phpmyadmin.gif" />
+
+    <Notice variant="warning">
+      ابزار phpMyAdmin برای بازیابی فایل‌های پشتیبان سنگین مناسب نیست. به‌همین
+      علت توصیه می‌کنیم به‌عنوان جایگزین از ابزار{" "}
+      <a href="#mysql-client">MySQL Command-Line Client</a> استفاده کنید.
+    </Notice>
+
+    <h4 id="mysql-client">MySQL Command-Line Client</h4>
+    <p>
+      در صورتی که قصد داشته باشید فایل پشتیبان را از طریق خط فرمان سیستم‌عامل
+      خود در دیتابیس‌های MySQL و MariaDB بازیابی کنید می‌توانید به‌شکل زیر از
+      ابزار <span className="code">mysql-client</span> استفاده کنید.
+    </p>
     <Highlight className="bash">
       {`$ mysql -u DB_USER -pDB_PASS -h DB_HOST -P DB_PORT < /path/to/backup-file.sql`}
     </Highlight>
