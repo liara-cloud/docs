3828cd643fe6d788587bad0deb7c13cf5f80af06
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-02-06 12:04:09 +0330

    Restore backup file into postgresql with pgadmin (#233)

--- pages/databases/postgresql/restore.js
+++ pages/databases/postgresql/restore.js
@@ -4,6 +4,7 @@ import Layout from "../../../components/Layout";
 import Notice from "../../../components/Notice";
 import Asciinema from "../../../components/Asciinema";
 import PlatformIcon from "../../../components/PlatformIcon";
+import ZoomableImage from "../../../components/ZoomableImage";
 
 export default () => (
   <Layout>
@@ -23,13 +24,39 @@ export default () => (
     </div>
 
     <h3>بازیابی فایل پشتیبان</h3>
+
     <p>
-      برای بازگردانی فایل پشتیبان در دیتابیس‌های PostgreSQL می‌توانید به‌شکل زیر
-      از ابزار <span className="code">pg_restore</span> استفاده کنید.
+      ممکن است در زمان انتقال سرویس و یا پس از مواجه شدن با هرگونه Data
+      corruption تصمیم بگیرید فایل پشتیبان دیتابیس خود را بازیابی کنید.
+    </p>
+
+    <h4 id="pgadmin">pgAdmin</h4>
+
+    <p>
+      بازیابی فایل پشتیبان در دیتابیس‌های PostgreSQL یکی از قابلیت‌هایی است که
+      ابزار pgAdmin در اختیار شما قرار می‌‌دهد و شما می‌توانید به‌شکل زیر از این
+      ابزار برای بازیابی فایل‌های پشتیبان استفاده کنید.
+    </p>
+
+    <ZoomableImage src="https://files.liara.ir/docs/postgresql/restore-backup-into-postgresql-database-with-pgadmin.gif" />
+
+    <Notice variant="warning">
+      برای بازیابی فایل‌های پشتیبان سنگین توصیه می‌کنیم از ابزار{" "}
+      <a href="#pg-restore">pg_restore</a> استفاده کنید.
+    </Notice>
+
+    <h4 id="pg-restore">pg_restore</h4>
+    <p>
+      در صورتی که قصد داشته باشید فایل پشتیبان را از طریق خط فرمان سیستم‌عامل
+      خود در دیتابیس‌های PostgreSQL بازیابی کنید می‌توانید به‌شکل زیر از ابزار{" "}
+      <span className="code">pg_restore</span> استفاده کنید.
     </p>
 
     <Highlight className="bash">
-      {`$ pg_restore -h DB_HOST -p DB_PORT -U DB_USERNAME -F c --create -d postgres /path/to/backup-file.dump`}
+      {`$ pg_restore -h DB_HOST \\
+             -p DB_PORT \\
+             -U DB_USERNAME \\
+             -F c --create -d postgres /path/to/backup-file.dump`}
     </Highlight>
 
     <Asciinema id="466058" />
