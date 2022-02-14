79ec8b4b51f9253ded98459a5d8a5a1260348bda
userdehghani <m.a.dehghani609@gmail.com>
2022-02-14 14:43:09 +0330

    up

--- pages/app-deploy/nodejs/getting-started.js
+++ pages/app-deploy/nodejs/getting-started.js
@@ -44,10 +44,10 @@ export default () => (
       <li>8</li>
       <li>10</li>
       <li>12</li>
+      <li>14</li>
       <li>
-        <b>14 (پیش‌فرض)</b>
+        <b>16 (پیش‌فرض)</b>
       </li>
-      <li>16</li>
     </ul>
 
     <div className="platforms">
