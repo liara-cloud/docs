fddfa4bff8230dd7b63f94d7f3b30b62badcded5
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-02-22 11:09:02 +0330

    Minor fix (#253)

--- pages/app-deploy/laravel/tips.js
+++ pages/app-deploy/laravel/tips.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Link from "next/link";
 import Head from "next/head";
 import Highlight from "react-highlight";
 import Layout from "../../../components/Layout";
