2dde80fbc331f7bdee4980236c366f3432d84a80
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-05-07 14:47:53 +0430

    Improve pusher

--- pages/one-click-apps/pusher/install.js
+++ pages/one-click-apps/pusher/install.js
@@ -40,29 +40,5 @@ export default () => (
       یکتا برای برنامه‌ی خود درنظر بگیرید، همچنین پلن مورد نظر خود را انتخاب
       کنید و در آخر بر روی دکمه‌ی <b>ایجاد برنامه</b> کلیک کنید.
     </p>
-
-    <h4>
-      تنظیم متغیرهای <span className="code">DEFAULT_APP_ID</span>،{" "}
-      <span className="code">DEFAULT_APP_KEY</span>،{" "}
-      <span className="code">DEFAULT_APP_SECRET</span>
-    </h4>
-    <p>
-      شما در قدم اول به دو متغیر ایمن برای مقداردهی متغیرهای{" "}
-      <span className="code">DEFAULT_APP_KEY</span> و{" "}
-      <span className="code">DEFAULT_APP_SECRET</span> نیاز دارید بنابراین وارد
-      صفحه‌ی{" "}
-      <a href="https://www.lastpass.com/password-generator">
-        Password Generator
-      </a>{" "}
-      وبسایت LastPass شده و گزینه‌ی <b>Easy to read</b> را انتخاب و پسوردی که
-      ایجاد شده را با کلیک بر روی دکمه‌ی <b>Copy Password</b>، کپی کنید.
-    </p>
-    <ZoomableImage src="https://files.liara.ir/docs/pusher/lastpass-password-generator.png"></ZoomableImage>
-    <p>
-      در مرحله‌ی بعد وارد تب برنامه‌ها شده و برنامه‌ی Pusher خود را انتخاب کنید.
-      اکنون باید متغیرهای کپی شده را در بخش <b>تنظیمات متغیرها</b> در فیلدهای
-      مربوطه قرار دهید و درنهایت بر روی دکمه‌ی <b>ثبت تغییرات</b> کلیک کنید.
-    </p>
-    <ZoomableImage src="https://files.liara.ir/docs/pusher/configure-environment-variables-in-pusher-app-settings.gif"></ZoomableImage>
   </Layout>
 );
