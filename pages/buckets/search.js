1e2ec55f67401ea1068247087b3c2e6d09c12d40
Mohammad Amin <m.a.dehghani609@gmail.com>
2022-07-31 13:45:44 +0430

    Fix: Add react component (#328)

---
+++ pages/buckets/search.js
@@ -0,0 +1,15 @@
+import Layout from "../../components/Layout";
+import Notice from "../../components/Notice";
+import Head from "next/head";
+
+export default () => (
+  <Layout>
+    <Head>
+      {/* <title>اتصال دامنه به فضای ذخیره‌سازی ابری - سرویس ابری لیارا</title> */}
+    </Head>
+
+    {/* <h1>فضای ذخیره‌سازی ابری</h1> */}
+
+    {/* <h2>اتصال دامنه</h2> */}
+  </Layout>
+);
