cb4c576244ca020490484fc892349946d48874d3
Mohammad Amin <m.a.dehghani609@gmail.com>
2022-08-16 15:44:28 +0430

    Update video urls (#346)

---
+++ pages/app-deploy/php/cli.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Link from "next/link";
+import Highlight from "react-highlight";
+import Notice from "../../../components/Notice";
+import Layout from "../../../components/Layout";
+import PlatformIcon from "../../../components/PlatformIcon";
+
+export default () => (
+  <Layout>
+    <Head>
+      <title>
+        مستندات استقرار برنامه‌های PHP با استفاده از ابزار Liara CLI - سرویس
+        ابری لیارا
+      </title>
+    </Head>
+
+    <div className="page-head">
+      <PlatformIcon platform="php" />
+      <div className="page-title">
+        <h1>برنامه‌های PHP</h1>
+        <span className="page-description">(Liara CLI)</span>
+      </div>
+    </div>
+
+    <h3>استقرار با Liara CLI</h3>
+
+    <p>
+      در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی آموزشی زیر
+      ‌را مشاهده کنید.
+    </p>
+
+    <video
+      src="https://files.liara.ir/liara/php/php-cli.mp4"
+      controls="controls"
+      className="block w-full"
+      width="100%"
+    ></video>
+
+    <Link href="/app-deploy/php/envs">متوجه شدم، برو گام بعدی!</Link>
+  </Layout>
+);
