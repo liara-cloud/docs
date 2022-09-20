bdf445e676994cde324419fd413733d6e1e1e4b4
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-09-20 14:36:40 +0430

    Update video urls

---
+++ pages/app-deploy/flask/desktop.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import Link from "next/link";
+import Notice from "../../../components/Notice";
+import Layout from "../../../components/Layout";
+import PlatformIcon from "../../../components/PlatformIcon";
+
+export default () => (
+  <Layout>
+    <Head>
+      <title>
+        مستندات استقرار برنامه‌های Flask با استفاده از ابزار Liara Desktop -
+        سرویس ابری لیارا
+      </title>
+    </Head>
+
+    <div className="page-head">
+      <PlatformIcon platform="django" />
+      <div className="page-title">
+        <h1>پلتفرم Flask</h1>
+        <span className="page-description">(Flask Platform)</span>
+      </div>
+    </div>
+
+    <h3>استقرار با Liara Desktop</h3>
+
+    <Notice variant="info">
+      در حال حاضر تنها نسخه‌ی ویندوز ابزار Liara Desktop منتشر شده و برای
+      استقرار برنامه‌های Flask در سیستم‌عامل لینوکس و مک می‌توانید از ابزار{" "}
+      <Link href="/app-deploy/flask/cli">Liara CLI</Link> استفاده کنید.
+    </Notice>
+
+    <p>
+      در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی آموزشی زیر
+      ‌را مشاهده کنید.
+    </p>
+
+    <video
+      src="https://files.liara.ir/liara/flask/flask-desktop.mp4"
+      controls="controls"
+      className="block w-full"
+      width="100%"
+    ></video>
+
+    <Link href="/app-deploy/flask/cli">متوجه شدم، برو گام بعدی!</Link>
+  </Layout>
+);
