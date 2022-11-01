ad1e6d72c02f93e60c3d5a788cd16491091bde9a
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-11-01 15:17:28 +0330

    up

--- pages/dns/records.js
+++ pages/dns/records.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/Layout";
-import PlatformIcon from "../../components/PlatformIcon";
+import Label from "../../components/Label";
 
 export default () => (
   <Layout>
@@ -13,5 +13,161 @@ export default () => (
     <span className="page-description">(DNS)</span>
 
     <h3 id="records">رکوردهای DNS</h3>
+
+    <h5>سرویس DNS لیارا از رکوردهای DNS زیر پشتیبانی می‌کند:</h5>
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>A</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>AAAA</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>ALIAS</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>CNAME</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>MX</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Priority</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>SRV</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Priority</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Port</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Weight</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
+
+    <br />
+
+    <table className="endpoint-inputs">
+      <tr className="endpoint-inputs__group">
+        <td colSpan={3}>TXT</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+      <tr>
+        <td>Contents</td>
+        <td className="endpoint-inputs__description"></td>
+      </tr>
+    </table>
   </Layout>
 );
