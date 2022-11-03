fd291315449671c7c64a564204cadf737e379d26
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-11-03 15:02:07 +0330

    up

--- pages/dns/records.js
+++ pages/dns/records.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../components/Layout";
-import Label from "../../components/Label";
 
 export default () => (
   <Layout>
@@ -16,96 +14,134 @@ export default () => (
 
     <h5>سرویس DNS لیارا از رکوردهای DNS زیر پشتیبانی می‌کند:</h5>
 
+    <p>
+      با استفاده از رکورد A می‌توانید یک و یا چند IPv4 را به یک دامنه اختصاص
+      دهید.
+    </p>
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>A</td>
+        <td>A record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          example.com یا @
+        </td>
       </tr>
       <tr>
         <td>Contents</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          93.184.216.34
+        </td>
       </tr>
     </table>
 
-    <br />
+    <p>
+      با استفاده از رکورد AAAA می‌توانید یک و یا چند IPv6 را به یک دامنه اختصاص
+      دهید.
+    </p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>AAAA</td>
+        <td>AAAA record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          example.com یا @
+        </td>
       </tr>
       <tr>
         <td>Contents</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          2606:2800:220:1:248:1893:25c8:1946
+        </td>
       </tr>
     </table>
 
-    <br />
+    <p>
+      با استفاده از رکورد ALIAS می‌توانید یک و یا چند نام دامنه را به ریشه‌ی
+      دامنه و یا ساب‌دامنه اختصاص دهید.
+    </p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>ALIAS</td>
+        <td>ALIAS record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          example.com یا @
+        </td>
       </tr>
       <tr>
         <td>Contents</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          to.liara.zone
+        </td>
       </tr>
     </table>
 
-    <br />
+    <p>
+      با استفاده از رکورد CNAME می‌توانید یک و یا چند نام دامنه را به یک
+      ساب‌دامنه اختصاص دهید.
+    </p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>CNAME</td>
+        <td>CNAME record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          www.example.com یا @
+        </td>
       </tr>
       <tr>
         <td>Contents</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          to.liara.zone
+        </td>
       </tr>
     </table>
 
-    <br />
+    <p>با استفاده از رکورد MX می‌توانید آدرس ایمیل سرور مقصد را مشخص کنید.</p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>MX</td>
+        <td>MX record</td>
+        <td>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
         <td className="endpoint-inputs__description"></td>
       </tr>
       <tr>
@@ -118,18 +154,17 @@ export default () => (
       </tr>
     </table>
 
-    <br />
+    <p>
+      با استفاده از رکورد SRV می‌توانید نام دامنه را به یک سرویس ارتباط دهید.
+    </p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>SRV</td>
+        <td>SRV record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
         <td className="endpoint-inputs__description"></td>
       </tr>
       <tr>
@@ -150,23 +185,33 @@ export default () => (
       </tr>
     </table>
 
-    <br />
+    <p>
+      با استفاده از رکورد TXT می‌توانید محتوای دلخواه خود را به دامنه نسبت
+      دهید..
+    </p>
 
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
-        <td colSpan={3}>TXT</td>
+        <td>TXT record</td>
+        <td style={{ textAlign: "center" }}>مثال</td>
       </tr>
       <tr>
         <td>Name</td>
-        <td className="endpoint-inputs__description"></td>
-      </tr>
-      <tr>
-        <td>TTL</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          liara-challenge
+        </td>
       </tr>
       <tr>
         <td>Contents</td>
-        <td className="endpoint-inputs__description"></td>
+        <td
+          className="endpoint-inputs__description"
+          style={{ direction: "ltr", textAlign: "center" }}
+        >
+          b4a78b-5dcb70-799744-179f58
+        </td>
       </tr>
     </table>
   </Layout>
