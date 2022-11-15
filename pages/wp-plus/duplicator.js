74180fd06f0ddc60795d96fa99dc8d8355dc43a1
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-11-15 15:02:51 +0330

    up

--- pages/wp-plus/duplicator.js
+++ pages/wp-plus/duplicator.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Notice from "../../components/Notice";
 import Layout from "../../components/Layout";
 import PlatformIcon from "../../components/PlatformIcon";
@@ -7,7 +8,9 @@ import ZoomableImage from "../../components/ZoomableImage";
 export default () => (
   <Layout>
     <Head>
-      <title>مستندات بسته نصب آسان (duplicator) - سرویس ابری لیارا</title>
+      <title>
+        نصب قالب با استفاده از بسته نصب آسان (duplicator) - سرویس ابری لیارا
+      </title>
     </Head>
 
     <div className="page-head">
@@ -18,7 +21,7 @@ export default () => (
       </div>
     </div>
 
-    <h3>بسته نصب آسان (duplicator)</h3>
+    <h2>نصب قالب با استفاده از بسته نصب آسان (duplicator)</h2>
 
     <h4>فهرست عناوین:</h4>
     <ul className="mt-0">
@@ -31,9 +34,30 @@ export default () => (
     </ul>
 
     <h3 id="installer">آپلود بسته نصب آسان</h3>
-    <p></p>
+    <p>
+      برای آپلود بسته نصب آسان وارد برنامه شده و از منوی{" "}
+      <strong>دسترسی FTP</strong>، یک دسترسی FTP جدید بسازید. بعد از ایجاد
+      دسترسی FTP می‌توانید با استفاده از نرم‌افزارهایی مثل{" "}
+      <Link href="/storage/disks/ftp#filezilla">FileZilla</Link> یا{" "}
+      <Link href="/storage/disks/ftp#winscp">WinSCP</Link> به محتوای برنامه‌ی
+      WordPress دسترسی پیدا کنید.
+    </p>
+    <p>
+      حال باید تمامی فایل‌ها و پوشه‌ها را انتخاب کرده و آن‌ها را حذف کنید. سپس
+      می‌توانید بسته نصب آسان را در لوکال از حالت فشرده خارج کرده و فایل‌ها و
+      پوشه‌های بسته نصب آسان را آپلود کنید.
+    </p>
 
     <h3 id="config">نصب قالب</h3>
-    <p></p>
+    <p>
+      برای نصب بسته آسان باید فایل <span className="code">installer.php</span>{" "}
+      را با استفاده از زیردامنه‌ی رایگان <strong>iran.liara.run</strong> در
+      مرورگر باز کرده و پیکربندی‌های لازم را براساس راهنمای قالب انجام دهید.
+    </p>
+    <Notice variant="info">
+      برای مشاهده‌ی اطلاعات دیتابیس ایجاد شده می‌توانید وارد منوی سرویس{" "}
+      <strong>دیتابیس</strong> شوید و اطلاعات دیتابیس را از منوی{" "}
+      <strong>نحوه‌ی اتصال</strong> کپی کنید.
+    </Notice>
   </Layout>
 );
