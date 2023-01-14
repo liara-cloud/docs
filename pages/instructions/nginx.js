ec3fb5a607ad102d12b9395ddd0b0c81f1c44dae
Mohammadreza <mrvashian@gmail.com>
2023-01-14 13:51:13 +0330

    chore: update nginx log formatter

--- pages/instructions/nginx.js
+++ pages/instructions/nginx.js
@@ -79,9 +79,7 @@ http {
   include       /etc/nginx/mime.types;
   default_type  application/octet-stream;
 
-  log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                    '$status $body_bytes_sent "$http_referer" '
-                    '"$http_user_agent" "$http_x_forwarded_for"';
+  log_format  main  '$request_method $status $http_x_forwarded_for "$request_uri" "$http_referer" "$http_user_agent"';
 
   sendfile        on;
   keepalive_timeout  65;
