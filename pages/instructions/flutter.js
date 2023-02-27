780c81b6d03f118a762c0b7ed84ac691d76899fc
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-02-27 05:31:16 +0000

    chore: enhance flutter web dockerfile (#422)

--- pages/instructions/flutter.js
+++ pages/instructions/flutter.js
@@ -37,12 +37,28 @@ export default () => (
     </p>
     <Highlight className="dockerfile">
       {`#Stage 1 - Install dependencies and build the app
-FROM debian:latest AS build-env
+FROM debian:latest
+
+ARG DEBIAN_FRONTEND=noninteractive
 
 # Install flutter dependencies
 RUN apt-get update && \\
-    apt-get install -y curl git wget unzip libgconf-2-4 gdb libstdc++6 libglu1-mesa fonts-droid-fallback lib32stdc++6 python3 && \\
-    apt-get clean
+    apt-get install -y --no-install-recommends \\
+      curl \\
+      git \\
+      wget \\
+      unzip \\
+      ca-certificates \\
+      libgconf-2-4 \\
+      gdb \\
+      libstdc++6 \\
+      libglu1-mesa \\
+      fonts-droid-fallback \\
+      lib32stdc++6 \\
+      python3 \\
+    && \\
+    apt-get clean && \\
+    rm -rf /var/lib/apt/lists
 
 # Clone the flutter repo
 RUN git clone https://github.com/flutter/flutter.git /usr/local/flutter
@@ -50,7 +66,6 @@ RUN git clone https://github.com/flutter/flutter.git /usr/local/flutter
 # Set flutter path
 # RUN /usr/local/flutter/bin/flutter doctor -v
 ENV PATH="/usr/local/flutter/bin:/usr/local/flutter/bin/cache/dart-sdk/bin:\${PATH}"
-ENV HTTPS_PROXY="http://fodev.org:8118"
 
 # Run flutter doctor
 RUN flutter doctor -v
@@ -60,15 +75,14 @@ RUN flutter channel master && \\
     flutter config --enable-web
 
 # Copy files to container and build
-RUN mkdir /app/
-COPY . /app/
-WORKDIR /app/
+WORKDIR /app
+COPY . .
 RUN flutter pub get && \\
     flutter build web
 
 # Stage 2 - Create the run-time image
 FROM liararepo/static-platform:base
-COPY --from=build-env /app/build/web /usr/share/nginx/html`}
+COPY --from=0 /app/build/web /usr/share/nginx/html`}
     </Highlight>
     <p>
       در نهایت، CMD و یا ترمینال را در پوشه‌ای که{" "}
