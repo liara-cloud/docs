10ea08f7ceacfd0b2fddc554c9cdd128ab36f713
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-03-01 14:19:43 +0330

    chore: upgrade meilisearch to v0.31.1

--- indexer/configs/meilisearchConnection.js
+++ indexer/configs/meilisearchConnection.js
@@ -4,5 +4,5 @@ import { envConfig } from "./envConfig.js";
 
 export const client = new MeiliSearch({
   host: envConfig.MEILI_ROOT_URL,
-  headers: { "X-Meili-API-Key": envConfig.MEILI_PRIVATE_KEY },
+  apiKey: envConfig.MEILI_PRIVATE_KEY,
 });
