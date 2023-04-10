90a3e1aaa492e67dc261a50b4b4d8717c7952b0c
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-04-10 12:17:03 +0330

    Update ffmpeg platforms

--- pages/app-features/ffmpeg.js
+++ pages/app-features/ffmpeg.js
@@ -10,10 +10,14 @@ export default () => (
 
     <h3>ماژول FFMPEG</h3>
     <p>
-      در حال حاضر ماژول FFMPEG در پلتفرم‌های Laravel و Flask نصب شده و متغیر
-      محیطی <span className="code">FFPROBE_PATH</span> به‌صورت پیش‌فرض تنظیم شده
-      است.
+      در حال حاضر ماژول FFMPEG در پلتفرم‌های زیر نصب شده و متغیر محیطی{" "}
+      <span className="code">FFPROBE_PATH</span> به‌صورت پیش‌فرض تنظیم شده است.
     </p>
+    <ul>
+      <li>Laravel</li>
+      <li>Flask</li>
+      <li>NodeJS</li>
+    </ul>
     <Notice variant="info">
       چنانچه در پلتفرم دیگری به این ماژول نیاز دارید، می‌توانید از طریق{" "}
       <a href="https://console.liara.ir/tickets">تیکت</a> با ما در ارتباط باشید
