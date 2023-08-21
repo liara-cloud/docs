b465bc63726dd044e983f9a51a0b228c97ec961a
MohammadHasan Akbari <116190942+jarqvi@users.noreply.github.com>
2023-08-21 15:40:22 +0330

    Dns doc (#459)

--- pages/dns/records.js
+++ pages/dns/records.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
+import ZoomableImage from "../../components/ZoomableImage";
 
 export default () => (
   <Layout>
@@ -13,7 +14,31 @@ export default () => (
     <h3 id="records">رکوردهای DNS</h3>
 
     <h5>سرویس DNS لیارا از رکوردهای DNS زیر پشتیبانی می‌کند:</h5>
+    <ul className="mt-0">
+      <li>
+        <a href="#a-record">رکورد A</a>
+      </li>
+      <li>
+        <a href="#aaaa-record">رکورد AAAA</a>
+      </li>
+      <li>
+        <a href="#alias-record">رکورد ALIAS</a>
+      </li>
+      <li>
+        <a href="#cname-record">رکورد CNAME</a>
+      </li>
+      <li>
+        <a href="#mx-record">رکورد MX</a>
+      </li>
+      <li>
+        <a href="#srv-record">رکورد SRV</a>
+      </li>
+      <li>
+        <a href="#txt-record">رکورد TXT</a>
+      </li>
+    </ul>
 
+    <h3 id="a-record">رکورد A</h3>
     <p>
       با استفاده از رکورد A می‌توانید یک و یا چند IPv4 را به یک دامنه اختصاص
       دهید.
@@ -32,12 +57,14 @@ export default () => (
         <td className="endpoint-inputs__description">93.184.216.34</td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/a.png" />
 
+    <h3 id="aaaa-record">رکورد AAAA</h3>
     <p>
       با استفاده از رکورد AAAA می‌توانید یک و یا چند IPv6 را به یک دامنه اختصاص
       دهید.
     </p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>AAAA record</td>
@@ -54,12 +81,14 @@ export default () => (
         </td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/aaaa.png" />
 
+    <h3 id="alias-record">رکورد ALIAS</h3>
     <p>
       با استفاده از رکورد ALIAS می‌توانید یک و یا چند نام دامنه را به ریشه‌ی
       دامنه و یا ساب‌دامنه اختصاص دهید.
     </p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>ALIAS record</td>
@@ -74,12 +103,14 @@ export default () => (
         <td className="endpoint-inputs__description">to.liara.zone</td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/alias.png" />
 
+    <h3 id="cname-record">رکورد CNAME</h3>
     <p>
       با استفاده از رکورد CNAME می‌توانید یک و یا چند نام دامنه را به یک
       ساب‌دامنه اختصاص دهید.
     </p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>CNAME record</td>
@@ -94,9 +125,11 @@ export default () => (
         <td className="endpoint-inputs__description">to.liara.zone</td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/cname.png" />
 
+    <h3 id="mx-record">رکورد MX</h3>
     <p>با استفاده از رکورد MX می‌توانید آدرس ایمیل سرور مقصد را مشخص کنید.</p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>MX record</td>
@@ -115,11 +148,13 @@ export default () => (
         <td className="endpoint-inputs__description">10</td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/mx.png" />
 
+    <h3 id="srv-record">رکورد SRV</h3>
     <p>
       با استفاده از رکورد SRV می‌توانید نام دامنه را به یک سرویس ارتباط دهید.
     </p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>SRV record</td>
@@ -146,12 +181,14 @@ export default () => (
         <td className="endpoint-inputs__description">5</td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/srv.png" />
 
+    <h3 id="txt-record">رکورد TXT</h3>
     <p>
       با استفاده از رکورد TXT می‌توانید محتوای دلخواه خود را به دامنه نسبت
       دهید..
     </p>
-
     <table className="endpoint-inputs">
       <tr className="endpoint-inputs__group">
         <td>TXT record</td>
@@ -168,5 +205,7 @@ export default () => (
         </td>
       </tr>
     </table>
+    <br />
+    <ZoomableImage src="/static/record/txt.png" />
   </Layout>
 );
