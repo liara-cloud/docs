0bae9bd2defb7d862b2fb099b7368e9c79bfcf04
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-08-26 12:33:31 +0330

    feat: add description for deploy next project

--- pages/app-deploy/nextjs/cli.js
+++ pages/app-deploy/nextjs/cli.js
@@ -22,7 +22,23 @@ export default () => (
       </div>
     </div>
 
-    <h3>استقرار با Liara CLI</h3>
+    <h4>فهرست عناوین:</h4>
+    <ul className="mt-0">
+      <li>
+        <a href="#video">استقرار با Liara CLI</a>
+      </li>
+      <li>
+        <a href="#installing-liara-cli">نصب Liara CLI</a>
+      </li>
+      <li>
+        <a href="#login">ورود به حساب کاربری</a>
+      </li>
+      <li>
+        <a href="#deploy">اولین استقرار</a>
+      </li>
+    </ul>
+
+    <h3 id="video">استقرار با Liara CLI</h3>
 
     <p>
       در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی آموزشی زیر
@@ -35,6 +51,115 @@ export default () => (
       className="block w-full"
       width="100%"
     ></video>
+
+    <p>
+      <h3 id="installing-liara-cli">نصب Liara CLI</h3>
+      اگر Liara CLI را نصب ندارید می‌توانید با اجرای دستور زیر آن‌ را به‌راحتی
+      نصب کنید: <Link href="/cli/install">توضیحات بیشتر</Link>
+    </p>
+    <pre>
+      <code>{`$ npm install -g @liara/cli`}</code>
+    </pre>
+    <h3 id="login">ورود به حساب کاربری</h3>
+    <p>
+      برای ورود به حساب کاربری خود به وسیله Liara CLI کافیست دستور زیر را وارد
+      کنید و ایمیل و رمزعبوری که با آن حساب کاربری خود را ایجاد کرده‌اید را وارد
+      نمایید:
+    </p>
+    <pre>
+      <code>{`$ liara login`}</code>
+    </pre>
+    <h3 id="deploy">اولین استقرار</h3>
+    <Notice variant="warning">
+      توجه داشته باشید؛ در لیارا برنامه‌های NextJS به روش
+      <span className="code">Create-Next-App</span> ایجاد می‌شوند.
+    </Notice>
+    <p>
+      <b>گام اول)</b> کافیست به بخش{" "}
+      <a href="https://console.liara.ir/apps/create" target="_blank">
+        ایجاد برنامه‌ها در لیارا
+      </a>{" "}
+      بروید و با انتخاب پلتفرم NextJS و نوشتن شناسه برنامه‌ی موردنظرتان و در
+      نهایت انتخاب پلن، برنامه خود را ایجاد کنید. برای نمونه، ما در این آموزش
+      برای برنامه آزمایشی‌مان، شناسه nextjs-starter را انتخاب کردیم. همچنین شما
+      می‌توانید از طریق Liara CLI با دستور زیر برنامه‌ی خود را ایجاد کنید.
+    </p>
+    <Highlight className="json">{`liara create`}</Highlight>
+    <p>
+      <b>گام دوم)</b> داخل فایل
+      <span className="code">package.json</span>
+      یک فیلد با نام
+      <span className="code">scripts</span>
+      وجود دارد. شما باید
+      <b> حتما </b>
+      یک اسکریپت در این بخش با نام
+      <span className="code">start</span>
+      تعریف کنید.
+    </p>
+    <Highlight className="json">
+      {`{
+    "scripts": {
+      "dev": "next dev",
+      "build": "next build",
+      "start": "next start",
+    },
+      "dependencies": {
+        "next": "^11.1.0",
+        "react": "^17.0.2",
+        "react-dom": "^17.0.2"
+      }
+}`}
+    </Highlight>
+    <p>
+      بعد از این‌که برنامه‌ی شما آپلود شد، لیارا برای شما دستور
+      <span className="code">npm start</span>
+      را اجرا می‌کند. شما باید داخل این اسکریپت، دستوری بنویسید که باعث اجرا شدن
+      برنامه‌ی‌تان شود. قرار دهید. (مانند نمونه‌ی بالا)
+    </p>
+    <Notice variant="warning">
+      توجه داشته باشید که لیارا به‌طور خودکار در فرایند استقرار برنامه‌های
+      NextJS دستور <span className="code">npm run build</span> را اجرا می‌کند.
+    </Notice>
+    <p>
+      <b>گام سوم)</b> کافیست وارد ریشه برنامه‌ی‌تان شده و به وسیله دستور زیر
+      اولین استقرار خود را اجرا کنید. بعد از وارد کردن این دستور، از شما شناسه
+      برنامه‌ موردنظرتان پرسیده می‌شود و بعد از انتخاب شناسه، لیارا عملیات
+      استقرار را شروع می‌کند.
+    </p>
+    <pre>
+      <code>{`$ liara deploy`}</code>
+    </pre>
+    <p>
+      Liara CLI به صورت خودکار، تشخیص خواهد داد که برنامه‌ی شما را باید به عنوان
+      یک برنامه‌ی NextJS اجرا کند و عملیات استقرار را آغاز خواهد کرد. اما اگر
+      مشکلی در تشخیص وجود داشت، می‌توانید از دستور زیر استفاده کنید:
+    </p>
+    <pre>
+      <code>{`$ liara deploy --platform=next`}</code>
+    </pre>
+    <Notice variant="info">
+      برنامه‌ی شما حتما باید دارای فایل
+      <span className="code">package.json</span> باشد تا بتواند در لیارا مستقر
+      شود.
+    </Notice>
+    <Notice variant="info">
+      لیارا به صورت خودکار پکیج‌هایی که در فایل
+      <span className="code">package.json</span>
+      لیست شده‌اند را برای شما نصب می‌کند. پس نیازی ندارید که به دنبال اجرای
+      دستور
+      <span className="code">npm install</span>
+      باشید. از آن‌جایی که اجرای این دستور زمان‌بر است، برای سرعت بیشتر، این
+      دستور را روی سرورهای قدرتمندمان اجرا می‌کنیم تا زمان زیادی را منتظر
+      نمانید.
+    </Notice>
+    <p>
+      <b>گام چهارم و پایانی)</b> بعد از اجرای دستورات گام‌های قبلی، لیارا به
+      برنامه شما یک زیردامنه رایگان اختصاص می‌دهد که به وسیله آن می‌توانید مطمئن
+      شوید که برنامه‌ی‌تان به صورت صحیح به بستر لیارا منتقل شده است یا خیر. این
+      آدرس بر اساس شناسه برنامه‌ی شما است، برای نمونه:
+    </p>
+
+    <p dir="ltr">https://nextjs-starter.liara.run</p>
 
     <Link href="/app-deploy/nextjs/domain" className="next-page">
       متوجه شدم، برو گام بعدی!
