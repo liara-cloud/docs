e90b9dcbdacde4ee0eb0af17938faf224c19dc45
Amirali Gholi <aligholi8220@gmail.com>
2024-03-17 12:19:46 +0330

    Remove tag manager

--- components/Layout.js
+++ components/Layout.js
@@ -34,25 +34,7 @@ export default ({ children }) => {
           data-website-id="da7a8997-effb-43ea-a5e3-9a7627cc540e"
           src="https://meta.liara.ir/umami.js"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PGMNBH9V')`,
-          }}
-        />
       </Head>
-
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGMNBH9V"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-        }}
-      />
 
       <main>
         <Header setSearchOpen={setSearchOpen} />
