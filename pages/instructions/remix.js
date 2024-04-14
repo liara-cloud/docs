f4b9d23c3090233b399d05bd9227bc4bf0b6a439
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-14 15:18:20 +0330

    update remix docs

--- pages/instructions/remix.js
+++ pages/instructions/remix.js
@@ -18,28 +18,29 @@ export default () => (
       </div>
     </div>
     <p>
-      فریم‌ورک Remix این امکان را در اختیار شما قرار می‌دهد تا تجربه‌ی کاربری
-      سریع و مدرنی را برای کاربران خود خلق کنید. حال شما می‌توانید برنامه‌های
-      Remix را با ایجاد برنامه‌های{" "}
+      <a href="https://github.com/remix-run/remix">Remix</a> یک فریم‌ورک وب است
+      که به شما امکان می‌دهد که بر روی رابط کاربری برنامه خود تمرکز کنید و با
+      تکیه بر استانداردهای وب، یک تجربه کاربری آسان، منعطف و عالی ارائه دهید.
+      شما می‌توانید برنامه‌های Remix را با ایجاد برنامه‌های{" "}
       <Link href="/app-deploy/nodejs/getting-started">NodeJS</Link> بر روی لیارا
       دیپلوی کنید.
     </p>
     <p>
       توجه داشته باشید که برای دیپلوی برنامه‌های Remix نیازی به ایجاد تغییر در
       فایل <span className="code">package.json</span> نیست و لیارا به‌طور کامل
-      از این فریم‌ورک پشتیبانی می‌کند بنابراین تغییری در بخش{" "}
+      از این فریم‌ورک پشتیبانی می‌کند؛ بنابراین تغییری در بخش{" "}
       <span className="code">scripts</span> ایجاد نکنید.
     </p>
     <Highlight className="json">{`"scripts": {
-  "build": "remix build",
-  "dev": "remix dev",
-  "postinstall": "remix setup node",
-  "start": "remix-serve build"
+    "build": "remix vite:build",
+    "dev": "remix vite:dev",
+    "lint": "eslint --ignore-path .gitignore --cache --cache-location ./node_modules/.cache/eslint .",
+    "start": "remix-serve ./build/server/index.js",
+    "typecheck": "tsc"
 },`}</Highlight>
 
     <p>
-      حالت استاندارد npm scripts در برنامه‌های Remix به‌شکل بالا است. در نهایت
-      دستور
+      حالت استاندارد scripts در برنامه‌های Remix به‌شکل بالا است. در نهایت دستور
       <span className="code">liara deploy --port 3000 --platform node</span>
       را اجرا کنید تا برنامه‌ی شما به لیارا منتقل شده و اجرا شود.
     </p>
