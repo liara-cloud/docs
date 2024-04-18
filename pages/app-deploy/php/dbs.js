d6a65960d00988fbae20bd2915728ad23c070332
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-18 16:08:00 +0330

    updat php/dbs

--- pages/app-deploy/php/dbs.js
+++ pages/app-deploy/php/dbs.js
@@ -20,6 +20,17 @@ export default () => (
     </div>
 
     <h3>اتصال به دیتابیس‌ها</h3>
+    <h4>فهرست عناوین:</h4>
+    <ul className="mt-0">
+      <li>
+        <a href="#mysql">اتصال به MySQL/MariaDB</a>
+      </li>
+      <li>
+        <a href="#postgres">اتصال به PostgreSQL</a>
+      </li>
+    </ul>
+
+    <h4 id="mysql">MySQL/MariaDB</h4>
     <p>
       در صورتی که تمایلی به خواندن آموزش متنی ندارید می‌توانید ویدیوی آموزشی زیر
       ‌را مشاهده کنید.
@@ -30,49 +41,83 @@ export default () => (
       className="block w-full"
       width="100%"
     ></video>
-    <ul>
-      <li>
-        <b>MySQL</b>
-      </li>
-      <p>
-        روش‌ها و کتابخانه‌های مختلفی برای اتصال به MySQL در PHP وجود دارد. برای
-        نمونه اگر از کتابخانه‌{" "}
-        <a href="https://github.com/catfan/Medoo" target="_blank">
-          Medoo
-        </a>{" "}
-        برای اتصال به دیتابیس و اجرای کوئری‌ها استفاده کرده‌اید، بدین صورت ENV
-        های لازم را اضافه می‌کنید:
-      </p>
-      <Highlight className="config">
-        {`DB_CONNECTION=mysql
-DB_HOST=s11.liara.ir
-DB_PORT=3306
-DB_DATABASE=php-starter-db
-DB_USERNAME=root
-DB_PASSWORD=xxxxxxxxxxxx
-`}
-      </Highlight>
-      حال کافیست در برنامه به این صورت اطلاعات را خوانده و به دیتابیس متصل شوید:
-      <Highlight className="php">
-        {`<?php
-require 'vendor/autoload.php';
-use Medoo\\Medoo;
 
-$database = new Medoo([
-        'database_type' => 'mysql',
-        'server' => getenv('DB_HOST'),
-        'port' => getenv('DB_PORT'),
-        'database_name' => getenv('DB_DATABASE'),
-        'username' => getenv('DB_USERNAME'),
-        'password' => getenv('DB_PASSWORD'),
-        'charset' => 'utf8'
-]);`}{" "}
-      </Highlight>
-    </ul>
-    <Notice variant="warning">
-      توجه داشته باشید که اطلاعات بالا همگی برای مثال است. شما متناسب با اطلاعات
-      دیتابیس برنامه‌ی‌تان باید مقادیر را جایگذاری کنید.
-    </Notice>
+    <p>
+      روش‌ها و کتابخانه‌های مختلفی برای اتصال به MySQL یا MariaDB در PHP وجود
+      دارد. برای در ادامه، ما از{" "}
+      <a href="https://www.php.net/manual/en/book.mysqli.php">mysqli</a> استفاده
+      می‌کنیم. در ابتدا، باید متغیرهای محیطی زیر را به برنامه خود اضافه کنید، در
+      نظر داشته باشید که مقادیر متغیرهای زیر فرعی هستند و شما باید آن‌ها را با
+      مقادیر اصلی دیتابیس خود، جایگزین کنید:
+    </p>
+    <Highlight className="config">
+      {`DB_HOST=annapurna.liara.cloud
+DB_PORT=34663
+DB_DATABASE=dreamy_zhukovsky
+DB_USERNAME=root
+DB_PASSWORD=Umtdo7rWEoEMyWth4nS1sf4t
+`}
+    </Highlight>
+    <p>
+      اکنون، می‌توانید با استفاده از قطعه کد زیر به دیتابیس MySQL/MariaDB خود
+      متصل شوید:
+    </p>
+
+    <Highlight className="php">
+      {`<?php
+$servername = getenv('DB_HOST');
+$port = getenv('DB_PORT'); 
+$username = getenv('DB_USERNAME'); 
+$password = getenv('DB_PASSWORD'); 
+$database = getenv('DB_DATABASE'); 
+
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected";}
+
+$conn->close();
+?>`}{" "}
+    </Highlight>
+
+    <h4 id="postgres">PostgreSQL</h4>
+    <p>
+      در ابتدا کافیست تا متغیرمحیطی زیر را به برنامه خود اضافه کنید؛ در نظر
+      داشته باشید که مقدار متغیر زیر، فرضی است:
+    </p>
+    <Highlight className="shell">
+      {`PG_URI=postgresql://root:2aCRtMfc2oMou67U2GftmLmd@annapurna.liara.cloud:32655/postgres`}
+    </Highlight>
+    <p>
+      در نهایت، می‌توانید با استفاده از قطعه کد زیر، به دیتابیس PostgreSQL خود
+      متصل شوید:
+    </p>
+    <Highlight className="php">
+      {`<?php
+$uri = getenv("PG_URI");
+
+try {
+    $uriParts = parse_url($uri);
+
+    $host = $uriParts['host'];
+    $port = $uriParts['port'];
+    $user = $uriParts['user'];
+    $pass = $uriParts['pass'];
+    $dbname = ltrim($uriParts['path'], '/');
+
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$pass");
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected to PostgreSQL database successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();}
+?>
+`}
+    </Highlight>
+
     <br />
 
     <Link href="/app-deploy/php/disks" className="next-page">
