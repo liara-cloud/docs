71f0576d8a01fb0c465112beb4cd1488672fcce0
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-05-06 14:58:40 +0330

    update app-features/build-location

--- pages/app-features/build-location.js
+++ pages/app-features/build-location.js
@@ -3,6 +3,7 @@ import Highlight from "react-highlight";
 
 import Layout from "../../components/Layout";
 import Notice from "../../components/Notice";
+import ZoomableImage from "../../components/ZoomableImage";
 
 export default () => (
   <Layout>
@@ -16,11 +17,11 @@ export default () => (
       را دانلود کنید یا نیاز داشته باشید که فرایند push کردن ایمیج‌ها به{" "}
       <a href="/app-features/private-registry/">رجیستری خصوصی</a>، با سرعت
       بیشتری انجام شود. برای این موارد می‌توانید موقعیت build ایمیج برنامه‌تان
-      را با یکی از دو روش زیر مشخص کنید:
+      را با یکی از سه روش زیر مشخص کنید:
     </p>
 
     <p>
-      ۱) یک فایل با نام
+      1) در استقرار با Liara CLI یک فایل با نام
       <span className="code">liara.json</span>
       در ریشه‌ پروژه‌تان ایجاد کرده و قطعه‌کد زیر را درون این فایل قرار بدید:
     </p>
@@ -35,7 +36,7 @@ export default () => (
     </Highlight>
 
     <p>
-      ۲) در دستور
+      2) در استقرار با Liara CLI در دستور
       <span className="code">liara deploy</span>
       موقعیت build را با پارامتر
       <span className="code">--build-location</span>
@@ -45,6 +46,12 @@ export default () => (
     <Highlight className="bash">
       {`liara deploy --build-location="germany"`}
     </Highlight>
+
+    <p>
+      3) در استقرار با مرورگر، موقعیت build را در بخش{" "}
+      <strong>تنظیمات build</strong> به شکل زیر انتخاب کنید:
+    </p>
+    <ZoomableImage src="/static/set-build-location.png"></ZoomableImage>
 
     <p>در حال حاضر موقعیت‌های زیر در دسترس هستند:</p>
     <ul>
