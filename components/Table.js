aa73fdb34f8026a2e43f5857a53f7fd979a3124d
Amirali Gholi <aligholi8220@gmail.com>
2024-06-09 12:02:37 +0330

    Add table component

---
+++ components/Table.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Table = ({ headers = [], data = [] }) => {
+  return (
+    <div
+      className="table-container"
+      style={{
+        background: "#ffffff08",
+        margin: "20px 0px",
+      }}
+    >
+      <table style={{ width: "100%", textAlign: "center" }}>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <td
+                key={cellIndex}
+                style={cellIndex === 1 ? { direction: "ltr" } : {}}
+              >
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+export default Table;
