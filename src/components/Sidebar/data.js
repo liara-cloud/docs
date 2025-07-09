8311b83b4ccac821c8747494a3169ec52ae64194
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-07-09 16:17:25 +0330

    docs: add ai/cookbook/multi-modal-chatbot

--- src/components/Sidebar/data.js
+++ src/components/Sidebar/data.js
@@ -363,22 +363,25 @@ export default {
         icon: <IconContainer alt="https://media.liara.ir/ai/ai-sdk/expo/expo-logo.png" />,
         link: "/ai/getting-started/expo"
       },
-      // {
-      //   hr: true
-      // },
-      // {
-      //   badge: (
-      //   <div className="flex items-center gap-2">
-      //     <IconContainer alt="https://media.liara.ir/ai/ai-sdk/ai-sdk-official-icon.png" />
-      //       راهنمای پخت‌وپز
-      //   </div>
-      //   )
-      // },
-
+      {
+        hr: true
+      },
+      {
+        badge: (
+        <div className="flex items-center gap-2">
+          <IconContainer alt="https://media.liara.ir/ai/ai-sdk/ai-sdk-official-icon.png" />
+            راهنمای پخت‌وپز
+        </div>
+        )
+      },
       // {
       //   title: "ساخت RAG Chatbot",
       //   link: "/ai/cookbook/rag-chatbot"
       // },
+      {
+        title: "ساخت چت بات چندمنظوره",
+        link: "/ai/cookbook/multi-modal-chatbot"
+      },
 
       {
         hr: true
