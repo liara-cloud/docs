f2abf5cb0bcbda54797df49454ec2b221c285e27
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-09-02 17:06:18 +0330

    chore: fix the important font color|

--- src/components/Common/important.js
+++ src/components/Common/important.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Important = ({ children }) => {
   return (
-    <span className="important mt-[1px] inline-block px-1 rounded mx-[3px] bg-[#0002] text-white">
+    <span className="important mt-[1px] inline-block px-1 rounded mx-[3px] bg-[#0002] text-black dark:text-white">
       {children}
     </span>
   );
