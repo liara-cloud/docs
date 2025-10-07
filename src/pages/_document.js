1bafcd7d2d5d4f09b861b14181cf5a0d50885dce
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-10-07 10:51:28 +0330

    seo: add GTM tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,19 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+
+				{/* Google Tag Manager */}
+				<script
+				dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-5C8DVF39');`,
+				}}
+				/>
+				{/* End Google Tag Manager */}
+
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta
 					property="og:image"
@@ -10,6 +23,18 @@ export default function Document() {
 				/>
 			</Head>
 			<body>
+
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+				<iframe
+					src="https://www.googletagmanager.com/ns.html?id=GTM-5C8DVF39"
+					height="0"
+					width="0"
+					style={{ display: "none", visibility: "hidden" }}
+				></iframe>
+				</noscript>
+				{/* End Google Tag Manager (noscript) */}
+
 				<Main />
 				<NextScript />
 			</body>
